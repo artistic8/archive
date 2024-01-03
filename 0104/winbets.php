5f4cce1f04e4cc4f729838577687a4621dee560b
artistic3 <artisticsoftware3@gmail.com>
2024-01-03 08:16:58 +0000

    ...

---
+++ 0104/winbets.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Favorite'    =>  '1',
+		'inter' =>  '5, 8',
+		'Place' =>  '8',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Favorite'    =>  '10',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Favorite'    =>  '5',
+		'inter' =>  '9',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Favorite'    =>  '2',
+		'inter' =>  '7, 9, 1',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Favorite'    =>  '5',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Favorite'    =>  '11',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Favorite'    =>  '7',
+		'inter' =>  '2',
+		'Place' =>  '2',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Favorite'    =>  '2',
+		'inter' =>  '6, 11',
+		'Place' =>  '6, 11',
+	],
+];
