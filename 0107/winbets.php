b21a4d5a22faefe1e23eee1a18812eedfeb11ba4
artistic3 <artisticsoftware3@gmail.com>
2024-01-07 09:06:08 +0000

    ...

--- 0107/winbets.php
+++ 0107/winbets.php
@@ -6,34 +6,29 @@ return [
 		Race 1
 		*/
 		'Favorite'    =>  '10',
-		'inter23' =>  '5, 2, 8, 7',
 		'inter' =>  '5, 8, 7',
-		'Place' =>  '8',
+		'Place' =>  '5',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'Favorite'    =>  '8',
-		'inter23' =>  '5, 9, 7',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'Favorite'    =>  '4',
-		'inter23' =>  '4, 3, 2',
 		'inter' =>  '4, 3, 2',
-		'Place' =>  '4, 2',
+		'Place' =>  '4',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'Favorite'    =>  '2',
-		'inter23' =>  '10, 4',
 		'inter' =>  '10',
-		'Place' =>  '10',
 	],
 	'5' => [
 		/**
@@ -46,25 +41,22 @@ return [
 		Race 6
 		*/
 		'Favorite'    =>  '4',
-		'inter23' =>  '3, 10',
 		'inter' =>  '3, 10',
-		'Place' =>  '10',
+		'Place' =>  '3',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'Favorite'    =>  '8',
-		'inter23' =>  '8, 2, 1, 7, 3, 4',
 		'inter' =>  '8, 2, 7',
-		'Place' =>  '8, 2',
+		'Place' =>  '8',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
 		'Favorite'    =>  '6',
-		'inter23' =>  '3, 5, 7',
 		'inter' =>  '3',
 	],
 	'9' => [
@@ -72,7 +64,6 @@ return [
 		Race 9
 		*/
 		'Favorite'    =>  '4',
-		'inter23' =>  '8, 5',
 	],
 	'10' => [
 		/**
