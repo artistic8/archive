f1bac457748e20b773736b81ec16ac500fcb4062
artistic3 <artisticsoftware3@gmail.com>
2024-01-12 13:50:16 +0000

    ...

--- archive/0110/winbets.php
+++ archive/0110/winbets.php
@@ -6,30 +6,26 @@ return [
 		Race 1
 		*/
 		'Favorite'    =>  '1',
-		'inter' =>  '2, 3',
-		'Place' =>  '2',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'Favorite'    =>  '2',
-		'inter' =>  '5, 7',
-		'Place' =>  '5',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'Favorite'    =>  '4',
-		'inter' =>  '11',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'Favorite'    =>  '4',
-		'inter' =>  '6',
+		'inter' =>  '6, 5',
+		'Place' =>  '6',
 	],
 	'5' => [
 		/**
@@ -52,8 +48,6 @@ return [
 		Race 7
 		*/
 		'Favorite'    =>  '5',
-		'inter' =>  '3, 2',
-		'Place' =>  '3',
 	],
 	'8' => [
 		/**
