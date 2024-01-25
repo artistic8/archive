566fe68df5f81ed23212cd1f9b9d08c28c6d5939
artistic3 <artisticsoftware3@gmail.com>
2024-01-25 03:21:31 +0000

    ...

---
+++ oknok.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+    1 => [
+        'Ok numbers' => [2, 3, 5, 7],
+        'Nok numbers' => [],
+        'Ok jockeys' => ['K Teetan', 'Y L Chung', 'Z Purton'],
+        'Nok jockeys' => [],
+    ],
+    2 => [
+        'Ok numbers' => [5, 7, 10],
+        'Nok numbers' => [1],
+        'Ok jockeys' => ['K Teetan', 'Z Purton'],
+        'Nok jockeys' => ['H Bowman'],
+    ],
+    3 => [
+        'Ok numbers' => [3, 4],
+        'Nok numbers' => [1, 9],
+        'Ok jockeys' => ['K Teetan', 'Z Purton'],
+        'Nok jockeys' => ['C Y Ho', 'H Bentley'],
+    ],
+    4 => [
+        'Ok numbers' => [1, 5, 7],
+        'Nok numbers' => [6],
+        'Ok jockeys' => ['C Y Ho', 'E C W Wong', 'Y L Chung'],
+        'Nok jockeys' => ['H Bowman'],
+    ],
+    5 => [
+        'Ok numbers' => [7],
+        'Nok numbers' => [4, 5],
+        'Ok jockeys' => ['K Teetan'],
+        'Nok jockeys' => ['K De Melo', 'Z Purton', 'L Hewitson'],
+    ],
+    6 => [
+        'Ok numbers' => [7],
+        'Nok numbers' => [3, 6],
+        'Ok jockeys' => ['A Badel', 'H Bowman', 'C Y Ho'],
+        'Nok jockeys' => ['Z Purton', 'L Ferraris'],
+    ],
+    7 => [
+        'Ok numbers' => [2, 6, 8],
+        'Nok numbers' => [3],
+        'Ok jockeys' => ['A Atzeni', 'A Badel', 'K Teetan', 'J McDonald'],
+        'Nok jockeys' => ['C Y Ho'],
+    ],
+    8 => [
+        'Ok numbers' => [5, 6],
+        'Nok numbers' => [2],
+        'Ok jockeys' => ['K Teetan', 'L Hewitson'],
+        'Nok jockeys' => ['H Bowman'],
+    ],
+    9 => [
+        'Ok numbers' => [],
+        'Nok numbers' => [],
+        'Ok jockeys' => [],
+        'Nok jockeys' => [],
+    ],
+    10 => [
+        'Ok numbers' => [],
+        'Nok numbers' => [],
+        'Ok jockeys' => [],
+        'Nok jockeys' => [],
+    ],
+    11 => [
+        'Ok numbers' => [],
+        'Nok numbers' => [],
+        'Ok jockeys' => [],
+        'Nok jockeys' => [],
+    ],
+];
+
+
+
+?>
