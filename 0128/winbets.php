da91a8556e644a92dc11815f78f0d9959b37eee3
artistic3 <artisticsoftware3@gmail.com>
2024-01-27 07:12:41 +0000

    ...

---
+++ 0128/winbets.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Favorite'    =>  '2',
+		'inter12' =>  '10, 8, 9, 3',
+		'inter' =>  '8, 3',
+		'Place' =>  '8',
+		// count > 2
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Favorite'    =>  '6',
+		'inter12' =>  '8, 1',
+		// count < 2
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Favorite'    =>  '2',
+		'inter12' =>  '2, 4, 6, 10',
+		'inter' =>  '2, 4, 10',
+		'Place' =>  '2',
+		// count > 2
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Favorite'    =>  '5',
+		'inter12' =>  '5, 6, 3, 4',
+		'inter' =>  '5, 6, 3, 4',
+		'Place' =>  '5',
+		// count > 2
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Favorite'    =>  '12',
+		'inter12' =>  '12, 2, 5, 8',
+		'inter' =>  '12, 2, 5, 8',
+		'Place' =>  '12',
+		// count > 2
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Favorite'    =>  '6',
+		'inter12' =>  '6, 2, 7, 4, 9',
+		'inter' =>  '6, 2, 7, 4, 9',
+		'Place' =>  '6',
+		// count > 2
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Favorite'    =>  '9',
+		'inter12' =>  '12, 11',
+		'inter' =>  '11',
+		// count < 2
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Favorite'    =>  '1',
+		'inter12' =>  '1, 9, 8, 2',
+		'inter' =>  '9, 8, 2',
+		'Place' =>  '9',
+		// count > 2
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'Favorite'    =>  '7',
+		// count < 2
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'Favorite'    =>  '7',
+		'inter12' =>  '7, 4',
+		// count < 2
+	],
+];
