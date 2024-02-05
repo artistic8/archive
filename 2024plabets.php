dc9a20da98b75234da8a85d46be69c886a2a6f3d
artistic3 <artisticsoftware3@gmail.com>
2024-02-05 06:10:30 +0000

    ...

--- 2024plabets.php
+++ 2024plabets.php
@@ -2,7 +2,7 @@
 
 if(!isset($argv[1])) die("Race Date Not Entered!!\n");
 
-$step = "plabets";
+$step = "2024plabets";
 $raceDate = trim($argv[1]);
 $currentDir = __DIR__ . DIRECTORY_SEPARATOR . $raceDate;
 
--- archive/0204/2024plabets.php
+++ archive/0204/2024plabets.php
@@ -5,7 +5,8 @@ return [
 		/**
 		Race 1
 		*/
-		'Favorite'    =>  '4',
+		'Favorite'  =>  '4',
+		'favorites' => '4',
 		'inter12' =>  '5',
 		// count < 2
 	],
@@ -13,41 +14,48 @@ return [
 		/**
 		Race 2
 		*/
-		'Favorite'    =>  '3',
+		'Favorite'  =>  '3',
+		'favorites' => '3',
 		'inter12' =>  '5',
 		'inter' =>  '5',
-		'Place' =>  '5',
+		'Place'  =>  '5',
+		'places' => '5',
 		// count < 2
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'Favorite'    =>  '6',
+		'Favorite'  =>  '6',
+		'favorites' => '6',
 		// count < 2
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'Favorite'    =>  '8',
+		'Favorite'  =>  '8',
+		'favorites' => '8',
 		'inter12' =>  '4',
 		'inter' =>  '4',
-		'Place' =>  '4',
+		'Place'  =>  '4',
+		'places' => '4',
 		// count < 2
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'Favorite'    =>  '3',
+		'Favorite'  =>  '3',
+		'favorites' => '3',
 		// count < 2
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'Favorite'    =>  '4',
+		'Favorite'  =>  '4',
+		'favorites' => '4',
 		'inter12' =>  '4, 12, 13',
 		// count < 2
 	],
@@ -55,28 +63,32 @@ return [
 		/**
 		Race 7
 		*/
-		'Favorite'    =>  '1',
+		'Favorite'  =>  '1',
+		'favorites' => '1',
 		// count < 2
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'Favorite'    =>  '6',
+		'Favorite'  =>  '6',
+		'favorites' => '6',
 		// count < 2
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
-		'Favorite'    =>  '11',
+		'Favorite'  =>  '11',
+		'favorites' => '11',
 		// count < 2
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
-		'Favorite'    =>  '10',
+		'Favorite'  =>  '10',
+		'favorites' => '10',
 		// count < 2
 	],
 ];
