442010ee22fc97d4cef655f91bd6a435e957acb7
artistic software <artisticsoftware41@gmail.com>
2024-02-06 08:17:59 +0000

    ...

--- 0207/arrangedbets.php
+++ 0207/arrangedbets.php
@@ -4,8 +4,8 @@ return [
 	'Race 7- Selected 3(odds: 1.5)',
 	'Race 4- Selected 1(odds: 1.9)',
 	'Race 5- Selected 2(odds: 2.1)',
-	'Race 1- Selected 2(odds: 3.8)',
-	'Race 3- Selected 11(odds: 3.8)',
-	'Race 9- Selected 11(odds: 5.5)',
-	'Race 8- Selected 9(odds: 5.7)',
+	'Race 1- Selected 2(odds: 3.6)',
+	'Race 3- Selected 11(odds: 3.7)',
+	'Race 9- Selected 11(odds: 5.3)',
+	'Race 8- Selected 9(odds: 5.8)',
 ];
