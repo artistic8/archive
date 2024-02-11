a0c4ef35d7cee505b11028dcd1777c728f4ec5eb
artistic software <artisticsoftware41@gmail.com>
2024-02-11 11:49:42 +0000

    ...

--- 0212/plabets.php
+++ 0212/plabets.php
@@ -24,6 +24,7 @@ return [
 		*/
 		'Favorite'  =>  '3',
 		'favorites' => '6, 3',
+		'interF' =>  '3, 12, 1, 4, 5',
 		'inter12' =>  '3, 12, 1, 4, 5',
 		'places' => '5, 3',
 		// count < 2
@@ -34,6 +35,7 @@ return [
 		*/
 		'Favorite'  =>  '2',
 		'favorites' => '2',
+		'interF' =>  '2, 1, 3, 10, 7, 11, 6, 14, 13, 12, 9, 8',
 		'inter12' =>  '2, 1, 3, 10, 7, 6, 12, 9, 8',
 		'inter' =>  '2, 1, 3, 10, 7, 6, 12, 9',
 		'Place'  =>  '2',
@@ -46,6 +48,7 @@ return [
 		*/
 		'Favorite'  =>  '12',
 		'favorites' => '12',
+		'interF' =>  '12, 4, 2, 6, 3, 8, 5',
 		'inter12' =>  '12, 4, 2, 6, 5',
 		'places' => '12',
 		// count < 2
@@ -56,6 +59,7 @@ return [
 		*/
 		'Favorite'  =>  '3',
 		'favorites' => '7, 3',
+		'interF' =>  '3, 4, 7, 11, 10, 2',
 		'inter12' =>  '8, 12, 11, 2',
 		'inter' =>  '8, 12, 2',
 		'Place'  =>  '8',
@@ -68,6 +72,7 @@ return [
 		*/
 		'Favorite'  =>  '4',
 		'favorites' => '4',
+		'interF' =>  '4, 8, 3, 9, 6, 7, 2, 11, 1, 5, 12',
 		'inter12' =>  '4, 3, 9, 2',
 		'inter' =>  '4, 9, 2',
 		'Place'  =>  '4',
@@ -80,6 +85,7 @@ return [
 		*/
 		'Favorite'  =>  '6',
 		'favorites' => '6',
+		'interF' =>  '6, 4, 3, 1',
 		'inter12' =>  '6, 4, 3, 1',
 		'inter' =>  '6, 3',
 		'Place'  =>  '6',
@@ -92,6 +98,7 @@ return [
 		*/
 		'Favorite'  =>  '8',
 		'favorites' => '8',
+		'interF' =>  '8, 5, 12, 1, 13, 11, 2, 10',
 		'inter12' =>  '8, 5, 1, 11, 10',
 		// count < 2
 	],
