60d65e08aeb7c7c27e935a5ef2691642a1944927
artistic5 <artisticsoftware5@gmail.com>
2024-02-15 00:45:26 +0000

    ...

--- 0215/arrangedbets1.php
+++ 0215/arrangedbets1.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-	'Race 2- Selected 3(odds: 1.4)',
+	'Race 2- Selected 3(odds: 1.5)',
 	'Race 8- Selected 8(odds: 1.5)',
 	'Race 1- Selected 9(odds: 1.6)',
 	'Race 7- Selected 3(odds: 1.8)',
 	'Race 9- Selected 1(odds: 2.6)',
-	'Race 4- Selected 6(odds: 2.9)',
+	'Race 4- Selected 6(odds: 2.8)',
 	'Race 3- Selected 3(odds: 3.4)',
 	'Race 6- Selected 2(odds: 4.5)',
 ];
