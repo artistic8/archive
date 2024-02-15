09dcdcebc9ef15041823cb0f254f254f62cecd35
artistic5 <122339035+artistic5@users.noreply.github.com>
2024-02-15 19:17:33 +0800

    Update plabets.php

--- plabets.php
+++ plabets.php
@@ -76,7 +76,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
         }
         asort($qplsOdds);
         $allTrioValuesF = array_keys($qplsOdds);
-        $allTrioValuesF = array_slice($allTrioValuesF, 0, 6);
+        //$allTrioValuesF = array_slice($allTrioValuesF, 0, 6);
         $unionF = array_values(array_unique(array_merge($unionF, $allTrioValuesF)));
         $racetext .= "\t\t'Trio values(Fav: $F)' =>  '" . implode(", ", $allTrioValuesF) . "',\n";
     }
