b3aa10f29a0470c65e1bde88804c1d6b9f2392db
Artistic Software <artisticsoftware7@gmail.com>
2024-02-27 07:45:38 +0000

    ...

---
+++ 0228/bets.php
@@ -0,0 +1,95 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Favorite'  =>  '9',
+		'favorites' => '9',
+		'winners' =>  '1, 3, 5, 6, 8, 9',//count: 6
+		'bet' =>  '1, 3, 5, 6, 8, 9',//count: 6
+		'historic' =>  '1, 3, 5, 6, 8, 9',//count: 6
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Favorite'  =>  '12',
+		'favorites' => '12',
+		'winners' =>  '3, 6, 10, 12',//count: 4
+		'bet' =>  '3, 6, 10, 12',//count: 4
+		'historic' =>  '3, 6, 10, 12',//count: 4
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Favorite'  =>  '9',
+		'favorites' => '9',
+		'winners' =>  '3, 6, 7, 8, 9',//count: 5
+		'bet' =>  '3, 6, 7, 8, 9',//count: 5
+		'historic' =>  '3, 6, 7, 8, 9',//count: 5
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Favorite'  =>  '5',
+		'favorites' => '5',
+		'winners' =>  '3, 4, 5, 6, 7, 12',//count: 6
+		'bet' =>  '3, 4, 5, 6, 7',//count: 5
+		'historic' =>  '3, 4, 5, 6, 7',//count: 5
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Favorite'  =>  '5',
+		'favorites' => '5',
+		'winners' =>  '1, 2, 3, 5, 6, 7',//count: 6
+		'bet' =>  '1, 2, 3, 5',//count: 4
+		'historic' =>  '1, 2, 3, 5',//count: 4
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Favorite'  =>  '2',
+		'favorites' => '2',
+		'winners' =>  '1, 2, 5, 7, 9, 10',//count: 6
+		'bet' =>  '1, 2, 5, 7, 9, 10',//count: 6
+		'historic' =>  '1, 2, 5, 7, 9, 10',//count: 6
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Favorite'  =>  '4',
+		'favorites' => '4',
+		'winners' =>  '1, 2, 4, 5, 9, 10',//count: 6
+		'bet' =>  '1, 2, 4, 5, 9',//count: 5
+		'historic' =>  '1, 2, 4, 5, 9',//count: 5
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Favorite'  =>  '5',
+		'favorites' => '5',
+		'winners' =>  '1, 2, 3, 4, 5, 7',//count: 6
+		'bet' =>  '1, 2, 3, 4, 5, 7',//count: 6
+		'historic' =>  '1, 2, 3, 4, 5, 7',//count: 6
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'Favorite'  =>  '1',
+		'favorites' => '1',
+		'winners' =>  '1, 3, 5, 8, 9, 11',//count: 6
+		'bet' =>  '1, 3, 5, 8',//count: 4
+		'historic' =>  '1, 3, 5, 8',//count: 4
+	],
+	//Total bets = 450 HKD
+];
