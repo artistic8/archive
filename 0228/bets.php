17b07152830c26e22d9791cf8a1f5ad3bdab8ea8
Artistic Software <artisticsoftware7@gmail.com>
2024-02-28 01:18:11 +0000

    ...

--- 0228/bets.php
+++ 0228/bets.php
@@ -8,16 +8,16 @@ return [
 		'Favorite'  =>  '9',
 		'favorites' => '9',
 		'winners' =>  '1, 3, 5, 6, 8, 9, 11',//count: 7
-		'bet' =>  '1, 3, 5, 6, 8, 9',//count: 6
+		'bet' =>  '1, 3, 5, 6, 9, 11',//count: 6
 		'historic' =>  '1, 3, 5, 6, 8, 9, 11',//count: 7
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'Favorite'  =>  '12',
-		'favorites' => '12',
-		'winners' =>  '1, 3, 6, 7, 10, 12',//count: 6
+		'Favorite'  =>  '3',
+		'favorites' => '12, 3',
+		'winners' =>  '1, 3, 6, 7, 10, 11, 12',//count: 7
 		'bet' =>  '1, 3, 6, 7, 12',//count: 5
 		'historic' =>  '1, 3, 6, 7, 10, 12',//count: 6
 	],
@@ -27,8 +27,8 @@ return [
 		*/
 		'Favorite'  =>  '3',
 		'favorites' => '9, 6, 3',
-		'winners' =>  '1, 3, 5, 6, 7, 8, 9, 12',//count: 8
-		'bet' =>  '1, 3, 5, 6, 8, 9',//count: 6
+		'winners' =>  '1, 3, 5, 6, 7, 8, 9, 10, 12',//count: 9
+		'bet' =>  '1, 3, 6, 8, 9',//count: 5
 		'historic' =>  '1, 3, 5, 6, 7, 8, 9, 12',//count: 8
 	],
 	'4' => [
@@ -37,9 +37,9 @@ return [
 		*/
 		'Favorite'  =>  '5',
 		'favorites' => '5',
-		'winners' =>  '3, 4, 5, 6, 7, 12',//count: 6
-		'bet' =>  '3, 4, 5, 6, 7',//count: 5
-		'historic' =>  '3, 4, 5, 6, 7',//count: 5
+		'winners' =>  '2, 3, 4, 5, 6, 7, 12',//count: 7
+		'bet' =>  '2, 3, 4, 5, 7',//count: 5
+		'historic' =>  '2, 3, 4, 5, 6, 7',//count: 6
 	],
 	'5' => [
 		/**
@@ -57,7 +57,7 @@ return [
 		*/
 		'Favorite'  =>  '2',
 		'favorites' => '2',
-		'winners' =>  '1, 2, 5, 7, 9, 10',//count: 6
+		'winners' =>  '1, 2, 5, 7, 9, 10, 12',//count: 7
 		'bet' =>  '1, 2, 5, 7, 9, 10',//count: 6
 		'historic' =>  '1, 2, 5, 7, 9, 10',//count: 6
 	],
@@ -67,18 +67,18 @@ return [
 		*/
 		'Favorite'  =>  '4',
 		'favorites' => '4',
-		'winners' =>  '1, 2, 3, 4, 5, 9, 10',//count: 7
-		'bet' =>  '1, 2, 3, 4, 5',//count: 5
-		'historic' =>  '1, 2, 3, 4, 5, 9',//count: 6
+		'winners' =>  '1, 2, 3, 4, 5, 7, 9, 10',//count: 8
+		'bet' =>  '1, 2, 3, 4, 7',//count: 5
+		'historic' =>  '1, 2, 3, 4, 5, 7, 9',//count: 7
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'Favorite'  =>  '5',
+		'Favorite'  =>  '2',
 		'favorites' => '5, 2',
 		'winners' =>  '1, 2, 3, 4, 5, 7, 8, 9',//count: 8
-		'bet' =>  '1, 2, 3, 4, 5, 7, 8',//count: 7
+		'bet' =>  '2, 3, 4, 5, 7, 8',//count: 6
 		'historic' =>  '1, 2, 3, 4, 5, 7, 8',//count: 7
 	],
 	'9' => [
@@ -88,8 +88,8 @@ return [
 		'Favorite'  =>  '1',
 		'favorites' => '1',
 		'winners' =>  '1, 2, 3, 5, 6, 7, 8, 9, 11',//count: 9
-		'bet' =>  '1, 3, 5, 6',//count: 4
-		'historic' =>  '1, 3, 5, 6, 7, 8, 11',//count: 7
+		'bet' =>  '1, 2, 3, 5, 6, 7',//count: 6
+		'historic' =>  '1, 2, 3, 5, 6, 7, 8, 11',//count: 8
 	],
 	//Total bets = 490 HKD
 ];
