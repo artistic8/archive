365fa8c0b42f3dfe325829b19d694156c551c189
Artistic Software <artisticsoftware7@gmail.com>
2024-02-28 12:50:58 +0000

    ...

--- 0228/bets.php
+++ 0228/bets.php
@@ -65,7 +65,7 @@ return [
 		/**
 		Race 7
 		*/
-		'Favorite'  =>  '4',
+		'Favorite'  =>  '3',
 		'favorites' => '4, 3',
 		'winners' =>  '1, 2, 3, 4, 5, 6, 7, 9, 10',//count: 9
 		'bet' =>  '1, 2, 3, 4, 7',//count: 5
