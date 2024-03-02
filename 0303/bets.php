d3f3c8d47d90cf6843598d9220ac3d460452527c
Artistic Software <artisticsoftware7@gmail.com>
2024-03-02 05:36:30 +0000

    ...

---
+++ 0303/bets.php
@@ -0,0 +1,127 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Favorite'  =>  '5',
+		'favorites' => '5',
+		'winners' =>  '5, 7, 8, 9, 10, 11',//count: 6
+		'bet' =>  '5, 7, 8, 9',//count: 4
+		'historic' =>  '5, 7, 8, 9',//count: 4
+		'WP' => '',
+		'WPs' => '',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Favorite'  =>  '9',
+		'favorites' => '9',
+		'winners' =>  '2, 3, 4, 6, 9, 14',//count: 6
+		'bet' =>  '2, 3, 4, 6, 9, 14',//count: 6
+		'historic' =>  '2, 3, 4, 6, 9, 14',//count: 6
+		'WP' => '9',
+		'WPs' => '9',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Favorite'  =>  '6',
+		'favorites' => '6',
+		'winners' =>  '1, 3, 4, 6, 9',//count: 5
+		'bet' =>  '1, 3, 4, 6, 9',//count: 5
+		'historic' =>  '1, 3, 4, 6, 9',//count: 5
+		'WP' => '',
+		'WPs' => '',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Favorite'  =>  '11',
+		'favorites' => '11',
+		'winners' =>  '4, 6, 9, 11, 12',//count: 5
+		'bet' =>  '4, 6, 9, 11',//count: 4
+		'historic' =>  '4, 6, 9, 11',//count: 4
+		'WP' => '11',
+		'WPs' => '11',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Favorite'  =>  '6',
+		'favorites' => '6',
+		'winners' =>  '1, 4, 5, 6, 9, 10',//count: 6
+		'bet' =>  '1, 4, 5, 6, 10',//count: 5
+		'historic' =>  '1, 4, 5, 6, 10',//count: 5
+		'WP' => '6',
+		'WPs' => '6',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Favorite'  =>  '6',
+		'favorites' => '6',
+		'winners' =>  '5, 6, 7, 8, 10, 11',//count: 6
+		'bet' =>  '5, 6, 7, 8, 10',//count: 5
+		'historic' =>  '5, 6, 7, 8, 10',//count: 5
+		'WP' => '6',
+		'WPs' => '6',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Favorite'  =>  '1',
+		'favorites' => '1',
+		'winners' =>  '1, 2, 4, 12, 13, 14',//count: 6
+		'bet' =>  '1, 2, 4, 12, 13, 14',//count: 6
+		'historic' =>  '1, 2, 4, 12, 13, 14',//count: 6
+		'WP' => '1',
+		'WPs' => '1',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Favorite'  =>  '4',
+		'favorites' => '4',
+		'winners' =>  '1, 3, 4, 5, 7, 10',//count: 6
+		'bet' =>  '1, 3, 4, 5, 10',//count: 5
+		'historic' =>  '1, 3, 4, 5, 10',//count: 5
+		'WP' => '4',
+		'WPs' => '4',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'Favorite'  =>  '6',
+		'favorites' => '6',
+		'winners' =>  '2, 4, 6, 8, 11, 13',//count: 6
+		'bet' =>  '2, 4, 6, 8, 11',//count: 5
+		'historic' =>  '2, 4, 6, 8, 11',//count: 5
+		'WP' => '6',
+		'WPs' => '6',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'Favorite'  =>  '11',
+		'favorites' => '11',
+		'winners' =>  '1, 6, 11',//count: 3
+		'bet' =>  '1, 6, 11',//count: 3
+		'historic' =>  '1, 6, 11',//count: 3
+		'WP' => '',
+		'WPs' => '',
+	],
+	//Total bets     = 480 HKD
+	//Total historic = 480 HKD
+	//Total winners  = 550 HKD
+];
