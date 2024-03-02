97a783a36600a0655769539020265b311c07a194
Artistic Software <artisticsoftware7@gmail.com>
2024-03-02 07:24:11 +0000

    ...

--- 0303/bets.php
+++ 0303/bets.php
@@ -7,9 +7,6 @@ return [
 		*/
 		'Favorite'  =>  '9',
 		'favorites' => '5, 7, 9',
-		'winners' =>  '1, 2, 5, 6, 7, 8, 9, 10, 11',//count: 9
-		'bet' =>  '1, 2, 5, 7, 9',//count: 5
-		'historic' =>  '1, 2, 5, 6, 7, 8, 9',//count: 7
 		'inter' => '',
 		'WP' => '',
 		'WPs' => '7',
@@ -20,9 +17,6 @@ return [
 		*/
 		'Favorite'  =>  '9',
 		'favorites' => '9',
-		'winners' =>  '2, 3, 4, 6, 9, 14',//count: 6
-		'bet' =>  '2, 3, 4, 6, 9, 14',//count: 6
-		'historic' =>  '2, 3, 4, 6, 9, 14',//count: 6
 		'inter' => '3, 6, 9',
 		'WP' => '9',
 		'WPs' => '9',
@@ -31,11 +25,8 @@ return [
 		/**
 		Race 3
 		*/
-		'Favorite'  =>  '9',
+		'Favorite'  =>  '6',
 		'favorites' => '6, 9, 7',
-		'winners' =>  '1, 2, 3, 4, 6, 7, 8, 9',//count: 8
-		'bet' =>  '1, 2, 3, 6, 7, 9',//count: 6
-		'historic' =>  '1, 2, 3, 4, 6, 7, 9',//count: 7
 		'inter' => '',
 		'WP' => '',
 		'WPs' => '',
@@ -46,9 +37,6 @@ return [
 		*/
 		'Favorite'  =>  '11',
 		'favorites' => '11',
-		'winners' =>  '4, 6, 9, 11, 12',//count: 5
-		'bet' =>  '4, 6, 9, 11, 12',//count: 5
-		'historic' =>  '4, 6, 9, 11, 12',//count: 5
 		'inter' => '6, 11',
 		'WP' => '11',
 		'WPs' => '11',
@@ -59,9 +47,6 @@ return [
 		*/
 		'Favorite'  =>  '6',
 		'favorites' => '6, 10',
-		'winners' =>  '1, 2, 4, 5, 6, 7, 9, 10',//count: 8
-		'bet' =>  '1, 2, 4, 5, 6, 10',//count: 6
-		'historic' =>  '1, 2, 4, 5, 6, 10',//count: 6
 		'inter' => '4',
 		'WP' => '',
 		'WPs' => '6',
@@ -72,9 +57,6 @@ return [
 		*/
 		'Favorite'  =>  '6',
 		'favorites' => '6',
-		'winners' =>  '3, 5, 6, 7, 8, 10, 11',//count: 7
-		'bet' =>  '3, 5, 6, 7',//count: 4
-		'historic' =>  '3, 5, 6, 7, 8, 10',//count: 6
 		'inter' => '1, 4, 6, 10, 11, 12',
 		'WP' => '6',
 		'WPs' => '6',
@@ -85,9 +67,6 @@ return [
 		*/
 		'Favorite'  =>  '1',
 		'favorites' => '1, 2',
-		'winners' =>  '1, 2, 4, 12, 13, 14',//count: 6
-		'bet' =>  '1, 2, 4, 12, 14',//count: 5
-		'historic' =>  '1, 2, 4, 12, 13, 14',//count: 6
 		'inter' => '2, 4, 5, 7, 9',
 		'WP' => '2',
 		'WPs' => '1, 2',
@@ -98,9 +77,6 @@ return [
 		*/
 		'Favorite'  =>  '5',
 		'favorites' => '4, 5',
-		'winners' =>  '1, 3, 4, 5, 7, 10',//count: 6
-		'bet' =>  '1, 3, 4, 5, 10',//count: 5
-		'historic' =>  '1, 3, 4, 5, 10',//count: 5
 		'inter' => '4, 7',
 		'WP' => '4',
 		'WPs' => '4',
@@ -111,9 +87,6 @@ return [
 		*/
 		'Favorite'  =>  '6',
 		'favorites' => '6',
-		'winners' =>  '2, 4, 6, 8, 11, 13',//count: 6
-		'bet' =>  '2, 4, 6, 8, 11',//count: 5
-		'historic' =>  '2, 4, 6, 8, 11',//count: 5
 		'inter' => '2, 3, 4, 6, 7',
 		'WP' => '6',
 		'WPs' => '6',
@@ -124,14 +97,8 @@ return [
 		*/
 		'Favorite'  =>  '11',
 		'favorites' => '11',
-		'winners' =>  '1, 3, 6, 11',//count: 4
-		'bet' =>  '1, 3, 6, 11',//count: 4
-		'historic' =>  '1, 3, 6, 11',//count: 4
 		'inter' => '',
 		'WP' => '',
 		'WPs' => '',
 	],
-	//Total bets     = 510 HKD
-	//Total historic = 570 HKD
-	//Total winners  = 650 HKD
 ];
--- bets.php
+++ bets.php
@@ -27,19 +27,11 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     if(!isset($allRacesOdds[$raceNumber])) continue;
     if(isset($oldData)){
         if(isset($oldData[$raceNumber]['favorites'])) $oldFavorites = explode(", ", $oldData[$raceNumber]['favorites']);
-        if(isset($oldData[$raceNumber]['winners'])) $oldWinners = explode(", ", $oldData[$raceNumber]['winners']);
-        if(isset($oldData[$raceNumber]['historic'])) $oldHistoric = explode(", ", $oldData[$raceNumber]['historic']);
         if(isset($oldData[$raceNumber]['WPs'])) $oldWPs = explode(", ", $oldData[$raceNumber]['WPs']);
     }
     if(isset($oldFavorites)) $favorites = $oldFavorites;
     else $favorites = [];
 
-    if(isset($oldWinners)) $winners = $oldWinners;
-    else $winners = [];
-
-    if(isset($oldHistoric)) $historic = $oldHistoric;
-    else $historic = [];
-    
     if(isset($oldWPs)) $WPs = $oldWPs;
     else $WPs = [];
     
@@ -58,8 +50,6 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     $racetext .= "\t\t'Favorite'  =>  '$favorite',\n";   
     $racetext .= "\t\t'favorites' => '" . implode(", ", $favorites) . "',\n";
     $win1 = $raceData1['win'];
-    $allTrioValues1 = [];
-    $unionW = $win1;
     $interW = $win1;
     foreach($favorites as $F){
         $raceDataF = $history[$raceNumber][$F];
@@ -72,84 +62,22 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
         asort($qplsOdds);
         $winF = array_keys($qplsOdds);
         //$racetext .= "\t\t'Win values(Fav: $F)' =>  '" . implode(", ", $winF) . "',\n";
-        $unionW = array_values(array_unique(array_merge($unionW, $winF)));
         $interW = array_intersect($interW, $winF);
     }
     
-    //Sort  unionW by odds
-    $qplsOdds = [];
-    foreach($unionW as $iIndex){
-       if(isset($allRacesOdds[$raceNumber][$iIndex])) $qplsOdds[$iIndex] = $allRacesOdds[$raceNumber][$iIndex];
-    }
-    asort($qplsOdds);
-    $unionW = array_keys($qplsOdds);
-    $missing = array_diff($runners, $unionW);
-    //Sort  missing by odds
-    $qplsOdds = [];
-    foreach($missing as $iIndex){
-       if(isset($allRacesOdds[$raceNumber][$iIndex])) $qplsOdds[$iIndex] = $allRacesOdds[$raceNumber][$iIndex];
-    }
-    asort($qplsOdds);
-    $missing = array_keys($qplsOdds);
-    //$racetext .= "\t\t'unionW(count: " . count($unionW) . ")' =>  '" . implode(", ", $unionW) . "',\n";
-    //$racetext .= "\t\t'missing(count: " . count($missing) . ")' =>  '" . implode(", ", $missing) . "',\n";
-    $newWinners = array_unique(array_values(array_merge(
-        array_slice($unionW, 0, 3),
-        array_slice($missing, 0, 3)
-    )));
-    $winners = array_unique(array_values(array_merge($winners, $newWinners)));
-    foreach($winners as $keyX => $horse){
-        if(!isset($allRacesOdds[$raceNumber][$horse])) unset($winners[$keyX]);
-    }
-    //sort $winners by odds
-    $qplsOdds = [];
-    foreach($winners as $iIndex){
-       if(isset($allRacesOdds[$raceNumber][$iIndex])) $qplsOdds[$iIndex] = $allRacesOdds[$raceNumber][$iIndex];
-    }
-    asort($qplsOdds);
-    $winners = array_keys($qplsOdds);
-    $set1 = array_slice($winners, 0, 7);
-    sort($winners);
-    $set2 = array_slice($winners, 0, 7);
-    $racetext .= "\t\t'winners' =>  '" . implode(", ", $winners) . "',//count: " . count($winners) . "\n";
-    $totalWinners += count($winners);
-    $set3 = array_intersect($set1, $set2);
-    $set4 = array_unique(array_values(array_merge(
-        array_slice($set2, 0, 4),
-        array_slice($set3, 0, 4)
-    )));
-    $set5 = array_intersect($set3, $set4);
-    $set5 = array_unique(array_values(array_merge($set5, $favorites)));
-    sort($set5);
-    $totalBets += count($set5);
-    $racetext .= "\t\t'bet' =>  '" . implode(", ", $set5) . "',//count: " . count($set5) . "\n";
-    $historic = array_unique(array_values(array_merge($historic, $set5)));
-    sort($historic);
-    $racetext .= "\t\t'historic' =>  '" . implode(", ", $historic) . "',//count: " . count($historic) . "\n";
     $racetext .= "\t\t'inter' => '" . implode(", ", $interW) . "',\n";
     $WP = array_intersect($interW, $favorites);
     $racetext .= "\t\t'WP' => '" . implode(", ", $WP) . "',\n";
     $WPs = array_unique(array_values(array_merge($WPs, $WP)));
     $racetext .= "\t\t'WPs' => '" . implode(", ", $WPs) . "',\n";
-    $totalHistoric += count($historic);
     $racetext .= "\t],\n";
     unset($oldFavorites);
-    unset($oldWinners);
-    unset($oldHistoric);
     unset($oldWPs);
     unset($favorites);
-    unset($winners);
-    unset($historic);
     unset($WPs);
     unset($interW);
     $outtext .= $racetext;
 }
-$totalBets = 10 * $totalBets;
-$totalHistoric = 10 * $totalHistoric;
-$totalWinners = 10 * $totalWinners;
-$outtext .= "\t//Total bets     = $totalBets HKD\n";
-$outtext .= "\t//Total historic = $totalHistoric HKD\n";
-$outtext .= "\t//Total winners  = $totalWinners HKD\n";
 $outtext .= "];\n";
 
 file_put_contents($outFile, $outtext);
