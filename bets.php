55041aa4fb0a27e4d86e2d66d160c307691b8590
artistic software <aadel.iimam@gmail.com>
2024-03-16 02:17:49 +0000

    ...

--- bets.php
+++ bets.php
@@ -23,25 +23,37 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     if(!isset($allRacesOdds[$raceNumber])) continue;
     if(isset($oldData)){
         if(isset($oldData[$raceNumber]['favorites'])) $oldFavorites = explode(", ", $oldData[$raceNumber]['favorites']);
+        if(isset($oldData[$raceNumber]['additional favorites'])) $oldAddedFavorites = explode(", ", $oldData[$raceNumber]['additional favorites']);
     }
     if(isset($oldFavorites)) $favorites = $oldFavorites;
     else $favorites = [];
 
+    if(isset($oldAddedFavorites)) $addedFavorites = $oldAddedFavorites;
+    else $addedFavorites = [];
+
     $sets = [];
-    $setsUnder6 = [];
 
     $winsArray = $allRacesOdds[$raceNumber];
     asort($winsArray);
     $runners = array_keys($winsArray);
     $favorite = $runners[0];
-    if(!in_array($favorite, $favorites)) $favorites[] = $favorite;
+    if(count($favorites) < 3){
+        if(!in_array($favorite, $favorites)) $favorites[] = $favorite;
+    }
+    else{
+        if(!in_array($favorite, $addedFavorites) && !in_array($favorite, $favorites)) $addedFavorites[] = $favorite;
+    }
     sort($runners);
     $racetext = "";
     $racetext .= "\t'$raceNumber' => [\n";
     $racetext .= "\t\t/**\n";
     $racetext .= "\t\tRace $raceNumber\n";
     $racetext .= "\t\t*/\n";
-    $racetext .= "\t\t'favorites' => '" . implode(", ", $favorites) . "',\n";   
+    $racetext .= "\t\t'favorites' => '" . implode(", ", $favorites) . "',\n"; 
+    if(!empty($addedFavorites))  {
+        $racetext .= "\t\t'additional favorites' => '" . implode(", ", $addedFavorites) . "',\n"; 
+    }
+    $favorites = array_merge($favorites, $addedFavorites);
     foreach($favorites as $one){
         if(isset($history[$raceNumber][$one]['win'])){
             $winners = $history[$raceNumber][$one]['win'];
@@ -57,10 +69,15 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
         }
         sort($union);
         $racetext .= "\t\t'union' => '" . implode(", ", $union) . "',//count: " . count($union) . "\n";
+        if(count($union) > 6 && count($favorites) < 6){
+            $racetext .= "\t\t'win/qin' => '" . implode(", ", $favorites) . "',\n";
+        }
     }
     $racetext .= "\t],\n";
     unset($oldFavorites);
     unset($favorites);
+    unset($oldAddedFavorites);
+    unset($addedFavorites);
     $outtext .= $racetext;
 }
 $outtext .= "];\n";
