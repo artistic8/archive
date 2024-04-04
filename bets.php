bd1950470b8352304fb6490854e90c99abd328de
artistic software <aadel.iimam@gmail.com>
2024-04-04 08:10:07 +0000

    ...

--- bets.php
+++ bets.php
@@ -87,8 +87,6 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
             $racetext .= "\t\t'union' => '" . implode(", ", $union) . "',\n";
             if(count($favorites) < 6 && !in_array(10, $favorites)) {
                 $racetext .= "\t\t'win/qqpl/trio' => '" . implode(", ", $favorites) . "',\n";
-                $hedge = array_diff($sums, $favorites);
-                $racetext .= "\t\t'wp(hedge)' => '" . implode(", ", $hedge) . "',\n";
             }
         }
     }
