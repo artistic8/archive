11b0a1b55a6473364022af0595f5b1b4aac78a8b
artistic software <aadel.iimam@gmail.com>
2024-04-08 06:53:48 +0000

    ...

---
+++ 0407/bets.php
@@ -0,0 +1,110 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '7, 12',
+		'official win' => '7, 13, 2',
+		'win hist' => '8, 10, 12',//count: 3
+		'all' => '7, 8, 10, 12',//count: 4
+		'place' => '12',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '4, 5, 10',
+		'official win' => '10, 14, 7',
+		'win hist' => '10',//count: 1
+		'qin/trio' => '4, 5, 10',
+		'all' => '4, 5, 10',//count: 3
+		'place' => '10',
+		'place' => '10',
+		'place' => '10',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '2',
+		'official win' => '2, 4, 6',
+		'win hist' => '1, 2, 5, 8',//count: 4
+		'all' => '1, 2, 5, 8',//count: 4
+		'place' => '5',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '4, 8',
+		'official win' => '2, 8, 5',
+		'win hist' => '1, 3, 8, 11',//count: 4
+		'all' => '1, 3, 4, 8, 11',//count: 5
+		'place' => '1',
+		'place' => '1',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '3, 6',
+		'official win' => '1, 4, 3',
+		'win hist' => '3, 4, 6, 7, 9',//count: 5
+		'all' => '3, 4, 6, 7, 9',//count: 5
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '1, 2, 4',
+		'official win' => '4, 1, 2',
+		'win hist' => '3, 6, 7, 8, 12, 13',//count: 6
+		'qin/trio' => '1, 2, 4',
+		'all' => '1, 2, 3, 4, 6, 7, 8, 12, 13',//count: 9
+		'place' => '12',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '1, 3',
+		'official win' => '1, 8, 5',
+		'win hist' => '1, 2, 3, 9, 10',//count: 5
+		'all' => '1, 2, 3, 9, 10',//count: 5
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '2, 3, 7',
+		'official win' => '3, 2, 6',
+		'win hist' => '1, 2, 3, 4, 5, 12',//count: 6
+		'qin/trio' => '2, 3, 7',
+		'all' => '1, 2, 3, 4, 5, 7, 12',//count: 7
+		'place' => '3',
+		'place' => '5',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '4, 7',
+		'official win' => '7, 2, 14',
+		'win hist' => '5, 6, 9, 12',//count: 4
+		'all' => '4, 5, 6, 7, 9, 12',//count: 6
+		'place' => '6',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '1, 3, 9',
+		'official win' => '10, 1, 3',
+		'win hist' => '2, 11',//count: 2
+		'qin/trio' => '1, 3, 9',
+		'all' => '1, 2, 3, 9, 11',//count: 5
+		'place' => '2',
+	],
+];
