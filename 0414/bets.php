2cc286aeceb88a03926e5f47227279557034a5fd
artistic software <aadel.iimam@gmail.com>
2024-04-14 05:07:11 +0000

    ...

---
+++ 0414/bets.php
@@ -0,0 +1,114 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '6, 7',
+		'win hist' => '1, 2, 3, 4, 5, 6, 7, 9',//count: 8
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2, 4, 10, 11',
+		'Favs 10, 1' => 'true',
+		'Favs 10, 7' => 'true',
+		'Favs 10, 10' => 'true',
+		'win hist' => '2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',//count: 11
+		'place' => '7, 10',
+		'bet' => '10',
+		'sure place' => '10',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '9, 13',
+		'Favs 9, 2' => 'true',
+		'Favs 9, 8' => 'true',
+		'Favs 9, 12' => 'true',
+		'Favs 13, 2' => 'true',
+		'Favs 13, 11' => 'true',
+		'win hist' => '2, 3, 7, 12',//count: 4
+		'place' => '2, 12',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '3, 4',
+		'Favs 3, 9' => 'true',
+		'Favs 3, 14' => 'true',
+		'Favs 4, 8' => 'true',
+		'win hist' => '2, 3, 4, 5, 6, 8, 9',//count: 7
+		'place' => '8, 9',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '5',
+		'Favs 5, 11' => 'true',
+		'win hist' => '1, 3, 4, 5, 12',//count: 5
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '3, 5, 9, 14',
+		'Favs 5, 1' => 'true',
+		'Favs 5, 12' => 'true',
+		'win hist' => '1, 2, 3, 5, 6, 7, 9, 11',//count: 8
+		'place' => '1',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '3, 7, 9',
+		'Favs 9, 2' => 'true',
+		'Favs 9, 3' => 'true',
+		'Favs 9, 4' => 'true',
+		'win hist' => '1, 2, 3, 4, 7, 8, 9, 10, 11, 12',//count: 10
+		'place' => '2, 3, 4',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '2, 3, 4, 6, 7, 8',
+		'win hist' => '1, 2, 3, 5, 6, 7, 8',//count: 7
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '2, 3',
+		'win hist' => '1, 2, 3, 4, 5, 6, 8, 9, 10, 11',//count: 10
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '1, 6, 10, 14',
+		'Favs 1, 3' => 'true',
+		'Favs 1, 5' => 'true',
+		'Favs 6, 1' => 'true',
+		'Favs 14, 2' => 'true',
+		'win hist' => '1, 2, 4, 5, 7, 9, 10, 13, 14',//count: 9
+		'place' => '1, 2, 5',
+		'bet' => '1',
+		'sure place' => '1',
+	],
+	'11' => [
+		/**
+		Race 11
+		*/
+		'favorites' => '2, 5, 9',
+		'Favs 5, 12' => 'true',
+		'win hist' => '1, 2, 12',//count: 3
+		'place' => '12',
+	],
+];
