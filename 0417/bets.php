3427b82a05a6bb69acb696276b349efd6e9c4f39
artistic software <aadel.iimam@gmail.com>
2024-04-18 02:45:02 +0000

    ...

---
+++ 0417/bets.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '5',
+		'win hist' => '1, 2, 5, 7, 8, 9, 12',//count: 7
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '1',
+		'win hist' => '2, 6, 7, 9',//count: 4
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '4, 9, 10',
+		'Favs 9, 8' => 'true',
+		'Favs 10, 5' => 'true',
+		'win hist' => '2, 3, 4, 5, 6, 7, 8, 9, 11, 12',//count: 10
+		'shit' => '5, 8, 9, 10',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '2, 6, 7, 11, 12',
+		'Favs 6, 10' => 'true',
+		'Favs 11, 1' => 'true',
+		'Favs 11, 7' => 'true',
+		'Favs 12, 1' => 'true',
+		'Favs 12, 7' => 'true',
+		'win hist' => '1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12',//count: 11
+		'shit' => '1, 6, 7, 10, 11, 12',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '4, 7',
+		'win hist' => '1, 2, 3, 4, 5, 6, 7, 8',//count: 8
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '1, 2, 3, 10',
+		'Favs 10, 3' => 'true',
+		'Favs 10, 6' => 'true',
+		'win hist' => '1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12',//count: 11
+		'shit' => '3, 6, 10',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '3, 6, 8',
+		'Favs 6, 9' => 'true',
+		'Favs 8, 2' => 'true',
+		'Favs 8, 9' => 'true',
+		'win hist' => '1, 2, 3, 6, 8, 9, 10',//count: 7
+		'shit' => '2, 6, 8, 9',
+		'gold' => '2',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '6, 8',
+		'win hist' => '1, 2, 3, 4, 6, 8, 9, 11, 12',//count: 9
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '4',
+		'win hist' => '4, 5, 6, 7, 9',//count: 5
+	],
+];
