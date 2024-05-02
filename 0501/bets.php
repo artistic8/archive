f8ef311fddc7fe09184397495042619f8c526a18
artistic software <aadel.iimam@gmail.com>
2024-05-02 02:38:47 +0000

    ...

---
+++ 0501/bets.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '1, 3, 4',
+		'inter' => '1, 4',
+		'win($20)' => '1, 3, 4',
+		'win($20)' => '3, 4',
+		'qin/trio($10)' => '1, 3, 4',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '3, 5, 6, 12',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '5',
+		'inter' => '5',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '3, 11',
+		'inter' => '11',
+		'potential inter(fav 6)' => '6, 11',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '3, 6',
+		'inter' => '3',
+		'potential inter(fav 1)' => '1, 3',
+		'potential inter(fav 4)' => '3, 4',
+		'potential inter(fav 7)' => '3, 7',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '4, 6, 10, 12',
+		'inter' => '12',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '3, 6, 11',
+		'inter' => '6',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '1, 2, 3, 4',
+		'inter' => '3',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '1, 7',
+		'inter' => '7',
+		'potential inter(fav 5)' => '5, 7',
+		'potential inter(fav 6)' => '6, 7',
+	],
+];
