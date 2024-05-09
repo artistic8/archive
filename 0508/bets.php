21de8ce89cf154cdff082d5e1582df698af4df56
artistic software <aadel.iimam@gmail.com>
2024-05-09 04:31:21 +0000

    ...

--- 0508/bets.php
+++ 0508/bets.php
@@ -6,28 +6,28 @@ return [
 		Race 1
 		*/
 		'favorites' => '6, 10',
-        'official win' => '',
+        'official win' => '6, 1, 8',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'favorites' => '5, 8, 10, 12',
-        'official win' => '',
+        'official win' => '12, 8, 1',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'favorites' => '1, 7, 10, 11',
-        'official win' => '',
+        'official win' => '11, 3, 10',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'favorites' => '1, 4, 7, 10',
-        'official win' => '',
+        'official win' => '5, 11, 7',
 		'inter' => '1, 2, 3, 4, 10, 11',
 	],
 	'5' => [
@@ -35,7 +35,7 @@ return [
 		Race 5
 		*/
 		'favorites' => '1, 2, 6',
-        'official win' => '',
+        'official win' => '1, 2, 12',
 		'inter' => '1, 2, 3, 4, 7',
 	],
 	'6' => [
@@ -43,14 +43,14 @@ return [
 		Race 6
 		*/
 		'favorites' => '2, 3, 4, 6, 7, 8, 10',
-        'official win' => '',
+        'official win' => '1, 7, 5',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'favorites' => '2, 3, 10',
-        'official win' => '',
+        'official win' => '5, 7, 2',
 		'win($20)' => '2, 3, 10',
 		'win($20)' => '3, 10',
 		'qin/trio($10)' => '2, 3, 10',
@@ -61,7 +61,7 @@ return [
 		Race 8
 		*/
 		'favorites' => '1, 5, 6',
-        'official win' => '',
+        'official win' => '10, 1, 6',
 		'inter' => '3, 4, 5, 6',
 	],
 	'9' => [
@@ -69,6 +69,6 @@ return [
 		Race 9
 		*/
 		'favorites' => '2, 9, 10',
-        'official win' => '',
+        'official win' => '4, 8, 10',
 	],
 ];
