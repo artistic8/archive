d984069b663d4ee62d6a47e55acf01c4df38bea0
artistic software <aadel.iimam@gmail.com>
2024-05-09 09:24:56 +0000

    ...

--- debug.sh
+++
@@ -1,9 +0,0 @@
-for f in *; do
-    if [ -d "$f" ] && [ $f != "archive" ] && [ $f != "random" ]; then
-        rm -Rf $f;
-        php populate.php;
-        git checkout $f/;
-        php bets.php $f;
-        git checkout winhistory.php;
-    fi
-done
