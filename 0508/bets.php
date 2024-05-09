1807e8020dd7adf1a78f8b7c707df9d74a3fb69f
artistic software <aadel.iimam@gmail.com>
2024-05-09 02:51:30 +0000

    ...

--- 0508/bets.php
+++ 0508/bets.php
@@ -6,24 +6,28 @@ return [
 		Race 1
 		*/
 		'favorites' => '6, 10',
+        'official win' => '',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'favorites' => '5, 8, 10, 12',
+        'official win' => '',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'favorites' => '1, 7, 10, 11',
+        'official win' => '',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'favorites' => '1, 4, 7, 10',
+        'official win' => '',
 		'inter' => '1, 2, 3, 4, 10, 11',
 	],
 	'5' => [
@@ -31,6 +35,7 @@ return [
 		Race 5
 		*/
 		'favorites' => '1, 2, 6',
+        'official win' => '',
 		'inter' => '1, 2, 3, 4, 7',
 	],
 	'6' => [
@@ -38,12 +43,14 @@ return [
 		Race 6
 		*/
 		'favorites' => '2, 3, 4, 6, 7, 8, 10',
+        'official win' => '',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'favorites' => '2, 3, 10',
+        'official win' => '',
 		'win($20)' => '2, 3, 10',
 		'win($20)' => '3, 10',
 		'qin/trio($10)' => '2, 3, 10',
@@ -54,6 +61,7 @@ return [
 		Race 8
 		*/
 		'favorites' => '1, 5, 6',
+        'official win' => '',
 		'inter' => '3, 4, 5, 6',
 	],
 	'9' => [
@@ -61,5 +69,6 @@ return [
 		Race 9
 		*/
 		'favorites' => '2, 9, 10',
+        'official win' => '',
 	],
 ];
