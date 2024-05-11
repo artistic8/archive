1a16b4c46524b0638ad31806a2de7bbe5080aaa5
artistic software <aadel.iimam@gmail.com>
2024-05-11 05:56:36 +0000

    ...

---
+++ 20230703/bets.php
@@ -0,0 +1,95 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '11',
+		'official win' => '4, 1, 11',
+		'inter' => '11',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2',
+		'official win' => '9, 6, 2',
+		'inter' => '2',
+		'Candidate(Favs 2, 1, 6)' => '2, 6',
+		'Candidate(Favs 2, 3, 6)' => '2, 6',
+		'Candidate(Favs 2, 4, 5)' => '4, 5',
+		'union' => '2, 4, 5, 6',//count: 4
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '4',
+		'official win' => '4, 3, 10',
+		'inter' => '4',
+		'Candidate(Favs 4, 1, 3)' => '3, 4',
+		'Candidate(Favs 4, 1, 5)' => '4, 5',
+		'Candidate(Favs 4, 3, 5)' => '3, 4',
+		'Candidate(Favs 4, 3, 6)' => '3, 4',
+		'Candidate(Favs 4, 5, 6)' => '4, 5',
+		'union' => '3, 4, 5',//count: 3
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '2',
+		'official win' => '2, 6, 9',
+		'inter' => '2',
+		'Candidate(Favs 2, 1, 4)' => '1, 2',
+		'Candidate(Favs 2, 1, 5)' => '1, 2, 5',
+		'union' => '1, 2, 5',//count: 3
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '3',
+		'official win' => '3, 6, 1',
+		'inter' => '3',
+		'Candidate(Favs 3, 1, 4)' => '1, 3, 4',
+		'Candidate(Favs 3, 1, 5)' => '1, 3, 5',
+		'Candidate(Favs 3, 1, 6)' => '1, 3',
+		'Candidate(Favs 3, 4, 5)' => '3, 4, 5',
+		'Candidate(Favs 3, 4, 6)' => '3, 4',
+		'union' => '1, 3, 4, 5',//count: 4
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '4',
+		'official win' => '7, 12, 6',
+		'inter' => '4',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '3',
+		'official win' => '3, 9, 4',
+		'inter' => '3',
+		'Candidate(Favs 3, 1, 2)' => '1, 2, 3',
+		'Candidate(Favs 3, 1, 4)' => '1, 3',
+		'Candidate(Favs 3, 2, 4)' => '2, 3',
+		'Candidate(Favs 3, 2, 6)' => '2, 6',
+		'union' => '1, 2, 3, 6',//count: 4
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '6',
+		'official win' => '6, 11, 3',
+		'inter' => '6',
+		'Candidate(Favs 6, 1, 4)' => '4, 6',
+		'Candidate(Favs 6, 2, 4)' => '4, 6',
+		'union' => '4, 6',//count: 2
+	],
+];
