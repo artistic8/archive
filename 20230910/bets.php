22acb120830140115f3fb2aa10688c1e22d81493
artistic software <aadel.iimam@gmail.com>
2024-05-11 06:50:52 +0000

    ...

---
+++ 20230910/bets.php
@@ -0,0 +1,123 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '8',
+		'official win' => '2, 8, 3',
+		'inter' => '8',
+		'Candidate(Favs 8, 1, 2)' => '1, 8',
+		'Candidate(Favs 8, 1, 5)' => '1, 5, 8',
+		'Candidate(Favs 8, 2, 3)' => '2, 3',
+		'Candidate(Favs 8, 2, 5)' => '2, 5, 8',
+		'union' => '1, 2, 3, 5, 8',//count: 5
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2',
+		'official win' => '6, 9, 5',
+		'inter' => '2',
+		'Candidate(Favs 2, 1, 6)' => '2, 6',
+		'Candidate(Favs 2, 3, 6)' => '2, 6',
+		'Candidate(Favs 2, 4, 5)' => '4, 5',
+		'union' => '2, 4, 5, 6',//count: 4
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '1',
+		'official win' => '2, 1, 7',
+		'inter' => '1',
+		'Candidate(Favs 1, 2, 3)' => '1, 3',
+		'Candidate(Favs 1, 3, 4)' => '3, 4',
+		'Candidate(Favs 1, 3, 6)' => '1, 3',
+		'Candidate(Favs 1, 4, 5)' => '4, 5',
+		'union' => '1, 3, 4, 5',//count: 4
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '1',
+		'official win' => '2, 1, 4',
+		'inter' => '1',
+		'Candidate(Favs 1, 2, 4)' => '1, 2',
+		'Candidate(Favs 1, 2, 5)' => '1, 2, 5',
+		'Candidate(Favs 1, 5, 6)' => '5, 6',
+		'union' => '1, 2, 5, 6',//count: 4
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '7',
+		'official win' => '5, 3, 6',
+		'inter' => '7',
+		'Candidate(Favs 7, 1, 2)' => '1, 7',
+		'Candidate(Favs 7, 1, 3)' => '1, 3, 7',
+		'Candidate(Favs 7, 1, 4)' => '1, 4, 7',
+		'Candidate(Favs 7, 1, 5)' => '1, 5',
+		'Candidate(Favs 7, 1, 6)' => '1, 7',
+		'Candidate(Favs 7, 3, 4)' => '3, 4, 7',
+		'Candidate(Favs 7, 3, 5)' => '3, 5',
+		'Candidate(Favs 7, 3, 6)' => '3, 7',
+		'Candidate(Favs 7, 4, 5)' => '4, 5',
+		'Candidate(Favs 7, 4, 6)' => '4, 7',
+		'union' => '1, 3, 4, 5, 7',//count: 5
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '4',
+		'official win' => '3, 4, 8',
+		'inter' => '4',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '8',
+		'official win' => '8, 9, 5',
+		'inter' => '8',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '8',
+		'official win' => '1, 9, 8',
+		'inter' => '8',
+		'Candidate(Favs 8, 1, 3)' => '1, 3',
+		'Candidate(Favs 8, 1, 4)' => '1, 4',
+		'Candidate(Favs 8, 2, 3)' => '3, 8',
+		'Candidate(Favs 8, 2, 4)' => '2, 4, 8',
+		'Candidate(Favs 8, 3, 4)' => '3, 8',
+		'Candidate(Favs 8, 3, 5)' => '3, 8',
+		'Candidate(Favs 8, 4, 5)' => '4, 8',
+		'union' => '1, 2, 3, 4, 8',//count: 5
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '1',
+		'official win' => '1, 10, 2',
+		'inter' => '1',
+		'Candidate(Favs 1, 5, 7)' => '5, 7',
+		'Candidate(Favs 1, 6, 7)' => '6, 7',
+		'union' => '5, 6, 7',//count: 3
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '6',
+		'official win' => '4, 6, 2',
+	],
+];
