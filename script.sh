79b59690136a1419195c20c74c4cc249d522b90a
artistic software <aadel.iimam@gmail.com>
2024-05-11 02:47:28 +0000

    ...

--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
 for f in *; do
     if [ -d "$f" ] && [ $f != "archive" ] && [ $f != "random" ]; then
-        # rm -Rf $f;
-        # php populate.php;
-        # git checkout $f/;
-        # php bets.php $f;
-        # git checkout winhistory.php;
-        cp $f/bets.php $f/alt.php
+        rm -Rf $f;
+        php populate.php;
+        git checkout $f/;
+        php bets.php $f;
+        git checkout winhistory.php;
     fi
 done
