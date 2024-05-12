da38f2427de321c00934b9eb3d3c6febec7c2d8e
artistic software <aadel.iimam@gmail.com>
2024-05-12 07:19:41 +0000

    ...

---
+++ 20240124/bets.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '3',
+		'official win' => '5, 6, 8',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '1',
+		'official win' => '2, 6, 5',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '3',
+		'official win' => '12, 4, 1',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '2',
+		'official win' => '13, 9, 3',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '4',
+		'official win' => '2, 12, 9',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '6',
+		'official win' => '12, 8, 5',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '2',
+		'official win' => '3, 6, 2',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '10',
+		'official win' => '9, 4, 2',
+	],
+];
