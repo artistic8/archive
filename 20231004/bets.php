b2eaf0963e3cf6f8841dcdfb06e8398f19fd57cc
artistic software <aadel.iimam@gmail.com>
2024-05-12 05:34:06 +0000

    ...

---
+++ 20231004/bets.php
@@ -0,0 +1,91 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '3',
+		'official win' => '7, 3, 8',
+		'inter' => '3',
+		'Candidate(Favs 3, 1, 4)' => '1, 4',
+		'Candidate(Favs 3, 1, 5)' => '1, 5',
+		'Candidate(Favs 3, 2, 5)' => '2, 5',
+		'union' => '1, 2, 4, 5',//count: 4
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '3',
+		'official win' => '2, 8, 3',
+		'Candidate(Favs 3, 2, 6)' => '2, 6',
+		'Candidate(Favs 3, 4, 5)' => '4, 5',
+		'union' => '2, 4, 5, 6',//count: 4
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '6',
+		'official win' => '4, 3, 6',
+		'inter' => '6',
+		'Candidate(Favs 6, 1, 3)' => '1, 3',
+		'Candidate(Favs 6, 3, 4)' => '3, 4',
+		'Candidate(Favs 6, 4, 5)' => '4, 5',
+		'union' => '1, 3, 4, 5',//count: 4
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '2',
+		'official win' => '2, 8, 11',
+		'inter' => '2',
+		'Candidate(Favs 2, 1, 4)' => '1, 2',
+		'Candidate(Favs 2, 1, 5)' => '1, 2, 5',
+		'union' => '1, 2, 5',//count: 3
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '8',
+		'official win' => '5, 7, 1',
+		'inter' => '8',
+		'Candidate(Favs 8, 1, 4)' => '1, 4',
+		'Candidate(Favs 8, 1, 5)' => '1, 5',
+		'Candidate(Favs 8, 1, 7)' => '1, 7',
+		'Candidate(Favs 8, 4, 5)' => '4, 5',
+		'Candidate(Favs 8, 4, 7)' => '4, 7',
+		'union' => '1, 4, 5, 7',//count: 4
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '1',
+		'official win' => '1, 5, 7',
+		'inter' => '1',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '1',
+		'official win' => '4, 11, 1',
+		'inter' => '1',
+		'Candidate(Favs 1, 2, 3)' => '1, 2, 3',
+		'Candidate(Favs 1, 2, 4)' => '1, 2, 4',
+		'Candidate(Favs 1, 2, 5)' => '2, 5',
+		'Candidate(Favs 1, 3, 4)' => '1, 3',
+		'union' => '1, 2, 3, 4, 5',//count: 5
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '12',
+		'official win' => '6, 5, 11',
+	],
+];
