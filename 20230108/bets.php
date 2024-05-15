7bd9febd74e495502277f0fbd24935f3ef616166
artistic software <aadel.iimam@gmail.com>
2024-05-15 02:29:45 +0000

    ...

---
+++ 20230108/bets.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '2, 14',
+		'official win' => '9, 3, 8',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '6',
+		'official win' => '4, 14, 1',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '9',
+		'official win' => '7, 5, 2',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '1, 6, 12',
+		'official win' => '3, 12, 6',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '4',
+		'official win' => '1, 10, 4',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '1, 3, 14',
+		'official win' => '5, 8, 12',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '1, 6',
+		'official win' => '5 8, 1',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '1',
+		'official win' => '11, 1, 12',
+	],
+    '9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '6, 11',
+		'official win' => '3, 13, 1',
+	],
+    '10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '5',
+		'official win' => '8, 2, 11',
+	],
+    '11' => [
+		/**
+		Race 11
+		*/
+		'favorites' => '6',
+		'official win' => '4, 6, 5',
+	],
+];
