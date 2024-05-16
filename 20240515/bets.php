9bbc2bb5ef3444c8ccfc1906755f2a077bd7398d
artistic software <aadel.iimam@gmail.com>
2024-05-16 02:48:33 +0000

    ...

--- 20240515/bets.php
+++ 20240515/bets.php
@@ -5,26 +5,26 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '1, 8, 10',
-		'win($20)' => '1, 8, 10',
-		'win($20)' => '8, 10',
-		'qin/trio($10)' => '1, 8, 10',
-		'union' => '1, 3, 4, 5, 7, 8, 10, 11',//count: 8
-		'inter' => '1, 8, 10',
+		'favorites' => '1, 7, 8, 10',
+		'official win' => '8, 4, 9',
+		'union' => '1, 2, 3, 4, 5, 6, 7, 8, 10, 11',//count: 10
+		'inter' => '1, 7, 8, 10',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'favorites' => '4, 9',
-		'union' => '3, 4, 8',//count: 3
-		'inter' => '4',
+		'favorites' => '2, 4, 8, 9',
+		'official win' => '4, 1, 6',
+		'union' => '2, 3, 4, 5, 6, 7, 8, 9',//count: 8
+		'inter' => '2, 4, 8, 9',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'favorites' => '1, 3',
+		'official win' => '7, 1, 5',
 		'union' => '1, 3, 4, 9',//count: 4
 		'inter' => '1, 3',
 	],
@@ -33,6 +33,7 @@ return [
 		Race 4
 		*/
 		'favorites' => '3, 6, 7',
+		'official win' => '3, 9, 1',
 		'union' => '3, 4, 5, 6, 10, 11',//count: 6
 		'inter' => '3, 6',
 	],
@@ -41,6 +42,7 @@ return [
 		Race 5
 		*/
 		'favorites' => '2, 4, 11',
+		'official win' => '11, 6, 7',
 		'union' => '1, 2, 3, 5, 7, 9',//count: 6
 		'inter' => '2',
 	],
@@ -49,6 +51,7 @@ return [
 		Race 6
 		*/
 		'favorites' => '4, 6, 9, 10',
+		'official win' => '8, 3, 9',
 		'union' => '1, 2, 3, 4, 6, 7, 9, 10, 12',//count: 9
 		'inter' => '4, 6, 9, 10',
 	],
@@ -57,6 +60,7 @@ return [
 		Race 7
 		*/
 		'favorites' => '3, 4',
+		'official win' => '7, 4, 6',
 		'union' => '1, 2, 3, 5, 10',//count: 5
 		'inter' => '3',
 	],
@@ -64,15 +68,17 @@ return [
 		/**
 		Race 8
 		*/
-		'favorites' => '3, 4',
-		'union' => '3, 6, 7, 8, 12',//count: 5
-		'inter' => '3',
+		'favorites' => '1, 3, 4',
+		'official win' => '4, 9, 8',
+		'union' => '1, 3, 4, 5, 6, 7, 8, 12',//count: 8
+		'inter' => '1, 3, 4',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'favorites' => '2, 3, 9',
+		'official win' => '2, 9, 7',
 		'union' => '2, 3, 4, 5, 8, 9, 12',//count: 7
 		'inter' => '2, 3, 9',
 	],
