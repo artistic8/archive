e16a731813000a7574e647a23cfe2e96a8b24456
artistic software <aadel.iimam@gmail.com>
2024-05-17 06:02:13 +0000

    ...

--- 20240414/bets.php
+++ 20240414/bets.php
@@ -7,8 +7,6 @@ return [
 		*/
 		'favorites' => '6, 7',
 		'official win' => '6, 7, 4',
-		'union' => '1, 2, 3, 4, 6, 7',//count: 6
-		'inter' => '6, 7',
 	],
 	'2' => [
 		/**
@@ -16,8 +14,6 @@ return [
 		*/
 		'favorites' => '2, 4, 10, 11',
 		'official win' => '7, 1, 5',
-		'union' => '1, 3, 4, 5, 6, 7, 8, 9, 10, 12',//count: 10
-		'inter' => '4, 10',
 	],
 	'3' => [
 		/**
@@ -25,8 +21,6 @@ return [
 		*/
 		'favorites' => '9, 13',
 		'official win' => '9, 10, 13',
-		'union' => '2, 7',//count: 2
-		'inter' => '',
 	],
 	'4' => [
 		/**
@@ -34,8 +28,6 @@ return [
 		*/
 		'favorites' => '3, 4',
 		'official win' => '3, 4, 10',
-		'union' => '1, 3, 4, 5, 6, 8',//count: 6
-		'inter' => '3, 4',
 	],
 	'5' => [
 		/**
@@ -50,9 +42,6 @@ return [
 		*/
 		'favorites' => '3, 5, 9, 14',
 		'official win' => '6, 11, 12',
-		'possible win' => '3, 5, 9',
-		'union' => '1, 2, 3, 5, 6, 7, 8, 9, 11, 13',//count: 10
-		'inter' => '3, 5, 9',
 	],
 	'7' => [
 		/**
@@ -60,8 +49,6 @@ return [
 		*/
 		'favorites' => '3, 7, 9',
 		'official win' => '6, 9, 3',
-		'union' => '1, 2, 3, 7, 8, 9, 12',//count: 7
-		'inter' => '3, 7, 9',
 	],
 	'8' => [
 		/**
@@ -69,11 +56,6 @@ return [
 		*/
 		'favorites' => '2, 3, 4, 6, 7, 8',
 		'official win' => '6, 8, 3',
-		'win($20)' => '2, 3, 4, 6, 7, 8',
-		'win($20)' => '3, 4',
-		'qin/trio($10)' => '2, 3, 4, 6, 7, 8',
-		'union' => '1, 2, 3, 5, 6, 7, 8',//count: 7
-		'inter' => '2, 3, 6, 7, 8',
 	],
 	'9' => [
 		/**
@@ -81,17 +63,13 @@ return [
 		*/
 		'favorites' => '2, 3',
 		'official win' => '8, 3, 9',
-		'union' => '2, 3, 5, 8, 9, 11',//count: 6
-		'inter' => '2, 3',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
-		'favorites' => '1, 6, 10, 14',
+		'favorites' => '1, 6, 10, 13, 14',
 		'official win' => '10, 2, 8',
-		'union' => '1, 4, 7, 8, 9, 10',//count: 6
-		'inter' => '1, 10',
 	],
 	'11' => [
 		/**
