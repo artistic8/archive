7d46bb96d41f3a485ceac1c13e40a646b847e699
artistic software <aadel.iimam@gmail.com>
2024-05-17 02:19:58 +0000

    ...

--- 20240511/bets.php
+++ 20240511/bets.php
@@ -14,8 +14,6 @@ return [
 		*/
 		'favorites' => '3, 5',
 		'official win' => '9, 4, 3',
-		'union' => '2, 3, 4, 5, 6',//count: 5
-		'inter' => '3, 5',
 	],
 	'3' => [
 		/**
@@ -23,8 +21,6 @@ return [
 		*/
 		'favorites' => '1, 5',
 		'official win' => '10, 1, 3',
-		'union' => '2, 3, 4, 5, 7, 8, 10',//count: 7
-		'inter' => '5',
 	],
 	'4' => [
 		/**
@@ -32,8 +28,6 @@ return [
 		*/
 		'favorites' => '1, 6, 8',
 		'official win' => '11, 1, 4',
-		'union' => '1, 2, 3, 4, 5, 6, 8, 10, 11, 12',//count: 10
-		'inter' => '1, 6, 8',
 	],
 	'5' => [
 		/**
@@ -41,8 +35,6 @@ return [
 		*/
 		'favorites' => '3, 6',
 		'official win' => '6, 3, 12',
-		'union' => '1, 3, 4, 7, 10, 11, 12',//count: 7
-		'inter' => '3',
 	],
 	'6' => [
 		/**
@@ -50,8 +42,6 @@ return [
 		*/
 		'favorites' => '1, 3, 12',
 		'official win' => '11, 7, 3',
-		'union' => '1, 2, 3, 4, 5, 6, 9, 10, 11, 12',//count: 10
-		'inter' => '1, 3, 12',
 	],
 	'7' => [
 		/**
@@ -59,8 +49,6 @@ return [
 		*/
 		'favorites' => '2, 7',
 		'official win' => '7, 8, 9',
-		'union' => '2, 4, 7',//count: 3
-		'inter' => '2, 7',
 	],
 	'8' => [
 		/**
@@ -68,8 +56,6 @@ return [
 		*/
 		'favorites' => '3, 7',
 		'official win' => '3, 13, 14',
-		'union' => '1, 3, 4, 5, 6, 7, 8, 12',//count: 8
-		'inter' => '3, 7',
 	],
 	'9' => [
 		/**
@@ -77,8 +63,6 @@ return [
 		*/
 		'favorites' => '4, 8',
 		'official win' => '1, 4, 8',
-		'union' => '1, 5, 12',//count: 3
-		'inter' => '',
 	],
 	'10' => [
 		/**
@@ -86,7 +70,5 @@ return [
 		*/
 		'favorites' => '2, 10',
 		'official win' => '7, 4, 9',
-		'union' => '8',//count: 1
-		'inter' => '',
 	],
 ];
---
+++ getresults.php
@@ -0,0 +1,44 @@
+<?php
+
+if(!isset($argv[1])) die("Race Date Not Entered!!\n");
+$raceDate = trim($argv[1]);
+$raceFolder = __DIR__ . DIRECTORY_SEPARATOR . $raceDate;
+if(!is_dir($raceFolder)) exec("mkdir $raceFolder");
+$betsFile = $raceFolder . DIRECTORY_SEPARATOR . "bets.php";
+$contents = include($betsFile);
+$raceDateFormat = substr($raceDate, 0, 4) . "/" . substr($raceDate, 4, 2) . "/" . substr($raceDate, 6, 2);
+$results =  $oddsJSON = file_get_contents("https://racing.hkjc.com/racing/information/English/Racing/ResultsAll.aspx?RaceDate=$raceDateFormat");
+$results = str_replace('<td class="f_fs14">', '', $results); 
+$results = str_replace(" ", '', $results); 
+$results = str_replace("\t", '', $results); 
+$results = str_ireplace("\x0D", "", $results); 
+$results = str_replace("</td>", '', $results); 
+$parts = explode("\n", $results);
+$tce = [];
+foreach($parts as $key => $part){
+    if(strpos($part, "TIERCE")) $tce[] = str_replace(",", ", ", $parts[$key + 1]);
+}
+$tce = array_values($tce);  
+$outtext = "<?php\n\n";
+$outtext .= "return [\n";
+
+foreach($contents as $raceNumber => $data){
+    $favorites = $data['favorites'];
+    $racetext = "";
+    $racetext .= "\t'$raceNumber' => [\n";
+    $racetext .= "\t\t/**\n";
+    $racetext .= "\t\tRace $raceNumber\n";
+    $racetext .= "\t\t*/\n";
+    $racetext .= "\t\t'favorites' => '" . $favorites . "',\n"; 
+    if(!isset($tce[$raceNumber - 1] )){
+        var_dump($raceDate);
+        var_dump($raceNumber);
+        var_dump($tce); die();
+    }
+    $racetext .= "\t\t'official win' => '" . $tce[$raceNumber - 1] ."',\n\t],\n"; 
+    $outtext .= $racetext;
+}
+$outtext .= "];\n";
+file_put_contents($betsFile, $outtext);
+
+?>
