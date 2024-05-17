c4acf49d4d6c194ad51fd05e5d960bc41c858aaa
artistic software <aadel.iimam@gmail.com>
2024-05-17 06:32:47 +0000

    ...

--- 20240303/bets.php
+++ 20240303/bets.php
@@ -5,62 +5,49 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '5, 7, 8',
+		'favorites' => '5, 7, 8, 9',
 		'official win' => '5, 2, 7',
-		'win($20)' => '5, 7, 8',
-		'win($20)' => '7, 8',
-		'qin/trio($10)' => '5, 7, 8',
-		'union' => '1, 2, 3, 4, 5, 6, 7, 8, 10',//count: 9
-		'inter' => '5, 7, 8',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'favorites' => '2, 3',
+		'favorites' => '3, 9',
 		'official win' => '2, 3, 14',
-		'union' => '2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13',//count: 11
-		'inter' => '2, 3',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'favorites' => '6, 9',
+		'favorites' => '6, 7, 9',
 		'official win' => '3, 1, 6',
-		'union' => '2, 4, 7',//count: 3
-		'inter' => '',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'favorites' => '4, 11',
+		'favorites' => '4, 9, 11, 12',
 		'official win' => '12, 4, 6',
-		'union' => '1, 4, 5, 6, 11',//count: 5
-		'inter' => '4, 11',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'favorites' => '6',
+		'favorites' => '6, 10',
 		'official win' => '11, 5, 3',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'favorites' => '6, 7, 10',
+		'favorites' => '6, 10',
 		'official win' => '7, 3, 6',
-		'union' => '1, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13',//count: 11
-		'inter' => '6, 10',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'favorites' => '1',
+		'favorites' => '1, 2',
 		'official win' => '1, 8, 4',
 	],
 	'8' => [
@@ -69,8 +56,6 @@ return [
 		*/
 		'favorites' => '4, 5',
 		'official win' => '8, 5, 10',
-		'union' => '1, 3, 4, 6, 7, 8, 10, 12',//count: 8
-		'inter' => '4',
 	],
 	'9' => [
 		/**
@@ -78,8 +63,6 @@ return [
 		*/
 		'favorites' => '2, 6',
 		'official win' => '4, 2, 13',
-		'union' => '1, 2, 3, 4, 5, 6, 8',//count: 7
-		'inter' => '2, 6',
 	],
 	'10' => [
 		/**
@@ -87,7 +70,5 @@ return [
 		*/
 		'favorites' => '6, 11',
 		'official win' => '1, 11, 2',
-		'union' => '8',//count: 1
-		'inter' => '',
 	],
 ];
