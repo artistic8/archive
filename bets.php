7004eb406360e5105024966eebd05fd9eb1e97f9
artistic8 <137678663+artistic8@users.noreply.github.com>
2024-05-20 20:02:17 +0800

    Update bets.php

--- bets.php
+++ bets.php
@@ -52,10 +52,13 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     if(isset($officialWin)){
         $racetext .= "\t\t'official win' => '" . implode(", ", $officialWin) . "',\n"; 
     }
+    foreach($runners as $R){
+        $candidates = array_intersect($history[$raceNumber][$R]["win"], $runners);
+        if(count($favorites) > 2 && empty(array_diff($favorites, $candidates))) $racetext .= "'hist(Runner $R)' => '" . implode(", ", $candidates) . "',\n";
+    }
     $firstSet = true;
     foreach($favorites as $F){
         $candidates = array_intersect($history[$raceNumber][$F]["win"], $runners);
-        if(count($favorites) > 2 && empty(array_diff($favorites, $candidates))) $racetext .= "'hist(Fav $F)' => '" . implode(", ", $candidates) . "',\n";
         if($firstSet) {
             $inter = $candidates;
             $firstSet = false;
