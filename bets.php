422576b99529775242f96d9a6923566281e54c1d
artistic software <aadel.iimam@gmail.com>
2024-05-21 02:58:32 +0000

    ...

--- bets.php
+++ bets.php
@@ -79,7 +79,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
         $racetext .= "\t\t'win($20)' => '" . implode(", ", $favorites) . "',\n"; 
         $racetext .= "\t\t'win($20)' => '" . implode(", ", array_slice($favorites, 1, 2)) . "',\n"; 
         $racetext .= "\t\t'qin/trio($10)' => '" . implode(", ", $favorites) . "',\n"; 
-        if(empty(array_diff($favorites, $inter))) $racetext .= "'sure bet' => true,\n";
+        if(empty(array_diff($favorites, $inter))) $racetext .= "\t\t'sure bet' => true,\n";
     }
  
     $racetext .= "\t],\n";
