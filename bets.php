553459d79aa50b9a8ec3ea388c5a5a4776bcf93c
artistic8 <137678663+artistic8@users.noreply.github.com>
2024-05-23 13:47:54 +0800

    Update bets.php

--- bets.php
+++ bets.php
@@ -70,12 +70,13 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     $racetext .= "\t\t'inter' => '" . implode(", ", $inter) . "',\n";
     $inter = array_intersect($favorites, $inter);
     $racetext .= "\t\t'inter' => '" . implode(", ", $inter) . "',\n";
+    if(count($favorites) - count($inter) === 1)  $racetext .= "\t\t'Place' => '" . implode(", ", $inter) . "',\n"; 
     if(count($inter) >= 2 && count($favorites) >= 3){
         $racetext .= "\t\t'win($20)' => '" . implode(", ", $favorites) . "',\n"; 
         $racetext .= "\t\t'win($20)' => '" . implode(", ", array_slice($favorites, 1, 2)) . "',\n"; 
         $racetext .= "\t\t'qin/trio($10)' => '" . implode(", ", $favorites) . "',\n"; 
     }
- 
+  
     $racetext .= "\t],\n";
     unset($oldFavorites);
     unset($favorites);
