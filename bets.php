c5aaa70099587807623157f5ef2ddab26dcd5836
artistic software <aadel.iimam@gmail.com>
2024-05-23 07:36:12 +0000

    ...

--- bets.php
+++ bets.php
@@ -72,7 +72,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     $diffInter = array_diff($trioInter, $winInter);
     $diffInter = array_intersect($diffInter, $favorites);
     $racetext .= "\t\t'diff inter' => '" . implode(", ", $diffInter) . "',\n"; 
-    if(empty($diffInter)) $racetext .= "\t\t//FAVORITES WIN OR AT LEAST PLACE???\n";
+    if(count($diffInter) === 1 && count($favorites) === 3) $racetext .= "\t\t'Place' => '" . implode(", ", $diffInter) . "',\n"; 
     $winInter = array_intersect($favorites, $winInter);
     $trioInter = array_intersect($favorites, $trioInter);
     if(count($winInter) >= 2 && count($favorites) >= 3){
