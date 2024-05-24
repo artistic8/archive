608180557b6e39f0ac838448dfb49d3876892b0b
artistic software <aadel.iimam@gmail.com>
2024-05-24 07:50:16 +0000

    ...

--- bets.php
+++ bets.php
@@ -85,14 +85,12 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     sort($trioUnion);
     $racetext .= "\t\t'trio union' => '" . implode(", ", $trioUnion) . "',\n";
 
-    $ratio3 = count($trioInter) / count($trioUnion);
     $ratio4 = count($qinInter) / count($winUnion);
     $ratio5 = count($trioInter) / count($winUnion);
     $ratio6 = count($winInter) / count($qinUnion);
     $ratio7 = count($trioInter) / count($winUnion);
 
-    $racetext .= "\t\t'ratio' => $ratio3,\n"; 
-    $racetext .= "\t\t'ratio4' => $ratio4,\n"; 
+    $racetext .= "\t\t'ratio' => $ratio4,\n"; 
     $racetext .= "\t\t'ratio5' => $ratio5,\n"; 
     $racetext .= "\t\t'ratio6' => $ratio6,\n"; 
     $racetext .= "\t\t'ratio7' => $ratio7,\n"; 
