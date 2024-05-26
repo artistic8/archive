4212bc0701f31951616a6577b9dc8a1528e1ce77
artistic8 <137678663+artistic8@users.noreply.github.com>
2024-05-26 18:43:26 +0800

    Update bets.php

--- bets.php
+++ bets.php
@@ -65,12 +65,12 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     }
     sort($union);
     sort($inter);
-    $racetext .= "\t\t'union' => '" . implode(", ", $union) . "',\n"; 
-    if(!empty($inter)) 
-    $racetext .= "\t\t'inter' => '" . implode(", ", $inter) . "',\n";
+    //$racetext .= "\t\t'union' => '" . implode(", ", $union) . "',\n"; 
+    //if(!empty($inter)) 
+  //  $racetext .= "\t\t'inter' => '" . implode(", ", $inter) . "',\n";
     $inter = array_intersect($favorites, $inter);
     if(count($inter) >= 2 && count($favorites) >= 3){
-        $racetext .= "\t\t'win($20)' => '" . implode(", ", $favorites) . "',\n"; 
+       // $racetext .= "\t\t'win($20)' => '" . implode(", ", $favorites) . "',\n"; 
         $racetext .= "\t\t'win($20)' => '" . implode(", ", array_slice($favorites, 1, 2)) . "',\n"; 
         $racetext .= "\t\t'qin/trio($10)' => '" . implode(", ", $favorites) . "',\n"; 
     }
