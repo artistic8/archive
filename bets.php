e7756e2cc92b18e7decb6de8338e57c8d7d5157c
artistic8 <137678663+artistic8@users.noreply.github.com>
2024-05-28 19:53:24 +0800

    Update bets.php

--- bets.php
+++ bets.php
@@ -100,8 +100,8 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
         $racetext .= "\t\t'win($20)' => '" . implode(", ", $favorites) . "',\n"; 
         $racetext .= "\t\t'win($20)' => '" . implode(", ", array_slice($favorites, 1, 2)) . "',\n"; 
         $racetext .= "\t\t'qin/trio($10)' => '" . implode(", ", $favorites) . "',\n"; 
-        $winbetAmount = 50 * count($favorites);
-        if(!empty(array_intersect($favorites, array_slice($officialWin, 0, 1)))) $winwonAmount = 5 * $winAmount - $winbetAmount;
+        $winbetAmount = 200 * count($favorites);
+        if(!empty(array_intersect($favorites, array_slice($officialWin, 0, 1)))) $winwonAmount = 20 * $winAmount - $winbetAmount;
         else $winwonAmount = 0 - $winbetAmount;
         $racetext .= "\t\t'win won amount' => $winwonAmount,\n";
         $totalWin += $winwonAmount;
