81a24a9d7103c7f5c51c4c9cc40b282410196682
artistic8 <137678663+artistic8@users.noreply.github.com>
2024-05-28 20:37:13 +0800

    Update bets.php

--- bets.php
+++ bets.php
@@ -112,13 +112,13 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
         $racetext .= "\t\t'qin won amount' => $qinwonAmount,\n";
         $totalQin += $qinwonAmount;
         $totalWonAmount += $qinwonAmount;
-    }
-     $f4betAmount = 10 * count($favorites) * combination(3, count($runners) - 1);
+        $f4betAmount = 10 * count($favorites) * combination(3, count($runners) - 1);
         if(!empty(array_intersect($favorites, $officialWin))) $f4wonAmount = $f4Amount - $f4betAmount;
         else $f4wonAmount = 0 - $f4betAmount;
         $racetext .= "\t\t'f4 won amount' => $f4wonAmount,\n";
         $totalF4 += $f4wonAmount;
         $totalWonAmount += $f4wonAmount;
+    }
     $racetext .= "\t],\n";
     unset($oldFavorites);
     unset($favorites);
