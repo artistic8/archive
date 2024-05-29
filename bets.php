4aa044df835f3fab20905b78fe56e947111c41ea
artistic software <aadel.iimam@gmail.com>
2024-05-29 08:03:25 +0000

    ...

--- bets.php
+++ bets.php
@@ -116,7 +116,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
         $total += $winwonAmount;
         $winbetAmount = 3 * $unitBet * count($set3);
         $totalWinBets += $winbetAmount;
-        $racetext .= "\t\t'total win bets' => $totalWinBets\n";
+        $racetext .= "\t\t'total win bets' => $totalWinBets,\n";
         if(!empty(array_intersect($set3, array_slice($officialWin, 0, 1)))) $winwonAmount = (3 * $unitBet / 10) *$winAmount - $winbetAmount;
         else $winwonAmount = 0 - $winbetAmount;
         $racetext .= "\t\t'win(set3)' => $winwonAmount,\n";
@@ -132,7 +132,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
         //qin 2: {favorites} X {set2}
         $qinbetAmount = 10 * count($favorites) * count($set2);
         $totalQinBets += $qinbetAmount;
-        $racetext .= "\t\t'total qin bets' => $totalQinBets\n";
+        $racetext .= "\t\t'total qin bets' => $totalQinBets,\n";
         if(count(array_intersect($favorites, array_slice($officialWin, 0, 2))) === 1 && count(array_intersect($set2, array_slice($officialWin, 0, 2))) === 1)
             $qinwonAmount = $qinAmount - $qinbetAmount;
         else $qinwonAmount = 0 - $qinbetAmount;
