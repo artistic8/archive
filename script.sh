7536e030c76cf1bf9c54b6a81aee81bc5716a778
artistic software <aadel.iimam@gmail.com>
2024-05-31 03:55:09 +0000

    ...

--- script.sh
+++ script.sh
@@ -2,8 +2,10 @@ for f in *; do
     if [ -d "$f" ] && [ $f != "archive" ] && [ $f != "oldFavorites" ]; then
         rm -Rf $f;
         php populate.php;
+        php inter3.php;
         git checkout $f/;
         php bets.php $f;
         git checkout history.php;
+        git checkout inter3.php;
     fi
 done
