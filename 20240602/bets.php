08b7f7951a8222ad14d7b38833aff325451048b3
artistic software <aadel.iimam@gmail.com>
2024-06-03 02:11:29 +0000

    ...

---
+++ 20240602/bets.php
@@ -0,0 +1,91 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '7, 9, 10',
+		'Place' => '7, 10',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2, 3',
+		'inter' => '2',//count: 1
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '3, 12',
+		'inter' => '12',//count: 1
+		'Place' => '12',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '1, 10',
+		'inter' => '1, 10',//count: 2
+		'Place' => '1, 10',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '1, 13',
+		'Place' => '1',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '2, 4',
+		'inter' => '4',//count: 1
+		'Place' => '2',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '1, 5, 6',
+		'Place' => '1, 6',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '4, 7, 8',
+		'inter' => '4, 8',//count: 2
+		'Place' => '4, 8',
+		'qin($20)' => '4, 7, 8',
+		'trio($10)' => '4, 7, 8',
+		'win($70)' => '4, 7, 8',
+		'win($70)' => '7, 8',
+		'total bets' => 420,
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '2, 9',
+		'inter' => '2',//count: 1
+		'Place' => '2, 9',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '3, 13',
+	],
+	'11' => [
+		/**
+		Race 11
+		*/
+		'favorites' => '4, 6, 7, 13',
+	],
+];
+//total place: 0
+//total: 0
