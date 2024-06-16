87f7411f09319701b09e6e1eaca8c26d8b735820
artistic software <aadel.iimam@gmail.com>
2024-06-16 08:46:41 +0000

    ...

--- condition.php
+++ condition.php
@@ -1,33 +1,33 @@
 <?php
 
 
-$favoriteWin = [5, 10, 24];
-$winInterWin = [];
-$qinInterWin = [24];
-$trioInterWin = [24];
-$favoritePlace = [5, 10, 14, 16, 18, 20, 24];
-$winInterPlace = [12, 16, 18, 20];
-$qinInterPlace = [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 24];
-$trioInterPlace = [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 24];
-$favoriteQin = [];
-$favoriteTrio = [];
+$favoriteWin = [20, 21, 25, 35, 40, 48];
+$winInterWin = [21];
+$qinInterWin = [21, 25, 35, 40, 48];
+$trioInterWin = [21, 25, 35, 40, 48];
+$favoritePlace = [3, 10, 15, 20, 21, 25, 28, 32, 35, 36, 40, 44, 48];
+$winInterPlace = [15, 16, 21, 24, 32, 35, 36, 40, 44];
+$qinInterPlace = [6, 8, 9, 10, 12, 15, 16, 18, 20, 21, 24, 25, 28, 30, 32, 35, 36, 40, 44, 48];
+$trioInterPlace = [1, 2, 3, 4, 6, 8, 9, 10, 12, 15, 16, 18, 20, 21, 24, 25, 28, 30, 32, 35, 36, 40, 44, 48];
+$favoriteQin = [25];
+$favoriteTrio = [25];
 $smallestFavoriteWin = [];
 $biggestFavoriteWin = [];
-$smallestFavoritePlace = [];
-$biggestFavoritePlace = [18];
+$smallestFavoritePlace = [3];
+$biggestFavoritePlace = [36];
 $placeCondition = [
-	1 => [14],
-	2 => [24],
-	3 => [20],
-	4 => [],
-	5 => [14],
-	6 => [],
-	7 => [14],
-	8 => [],
+	1 => [28, 35],
+	2 => [21, 48],
+	3 => [25, 35, 40, 44],
+	4 => [21, 25],
+	5 => [25, 28],
+	6 => [35],
+	7 => [28],
+	8 => [1, 44],
 	9 => [],
-	10 => [],
+	10 => [21],
 	11 => [],
-	12 => [],
+	12 => [44],
 	13 => [],
 	14 => [],
 ];
