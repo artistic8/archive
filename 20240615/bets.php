e6c6d2974094291c2e6eac4be9e546f5c62ef9f5
artistic software <aadel.iimam@gmail.com>
2024-06-16 09:36:07 +0000

    ...

--- 20240615/bets.php
+++ 20240615/bets.php
@@ -6,6 +6,7 @@ return [
 		Race 1
 		*/
 		'favorites' => '7, 11',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
 		'official win' => '8, 7, 4, 10',
 		'win amount' => 151.5,
 		'qin amount' => 276.5,
@@ -15,12 +16,19 @@ return [
 			7 => 14.5,
 			4 => 19.5,
 		],
+		'win inter' => '',
+		'qin inter' => '',
+		'trio inter' => '',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'favorites' => '3, 5, 6',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'official win' => '3, 1, 10, 4',
 		'win amount' => 56,
 		'qin amount' => 225,
@@ -30,12 +38,19 @@ return [
 			1 => 28,
 			10 => 42.5,
 		],
+		'win inter' => '5',
+		'qin inter' => '5, 6',
+		'trio inter' => '5, 6',
+		'win pivots' => '5, 6',
+		'qin pivots' => '5, 6',
+		'trio pivots' => '3, 5, 6',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'favorites' => '2, 3, 4, 9, 11, 12',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
 		'official win' => '9, 8, 4, 12',
 		'win amount' => 45.5,
 		'qin amount' => 659,
@@ -45,12 +60,19 @@ return [
 			8 => 66.5,
 			4 => 24,
 		],
+		'win inter' => '',
+		'qin inter' => '12',
+		'trio inter' => '12',
+		'win pivots' => '2, 3, 4, 9, 11, 12',
+		'qin pivots' => '2, 3, 4, 9, 11, 12',
+		'trio pivots' => '2, 3, 4, 9, 11, 12',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'favorites' => '1',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'official win' => '1, 3, 4, 12',
 		'win amount' => 16,
 		'qin amount' => 93,
@@ -60,12 +82,19 @@ return [
 			3 => 25.5,
 			4 => 14,
 		],
+		'win inter' => '1',
+		'qin inter' => '1',
+		'trio inter' => '1',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
 		'favorites' => '1, 3, 10',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'official win' => '3, 10, 6, 5',
 		'win amount' => 64.5,
 		'qin amount' => 416.5,
@@ -75,12 +104,19 @@ return [
 			10 => 36,
 			6 => 21.5,
 		],
+		'win inter' => '1',
+		'qin inter' => '1, 3, 10',
+		'trio inter' => '1, 3, 10',
+		'win pivots' => '1, 3, 10',
+		'qin pivots' => '1, 3, 10',
+		'trio pivots' => '1, 3, 10',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'favorites' => '2',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
 		'official win' => '2, 3, 10, 1',
 		'win amount' => 14.5,
 		'qin amount' => 41.5,
@@ -90,12 +126,19 @@ return [
 			3 => 14.5,
 			10 => 38.5,
 		],
+		'win inter' => '2',
+		'qin inter' => '2',
+		'trio inter' => '2',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'favorites' => '2, 3, 9',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'official win' => '10, 6, 2, 5',
 		'win amount' => 69,
 		'qin amount' => 516,
@@ -105,12 +148,19 @@ return [
 			6 => 31,
 			2 => 17,
 		],
+		'win inter' => '2',
+		'qin inter' => '2, 3, 9',
+		'trio inter' => '2, 3, 9',
+		'win pivots' => '2, 3, 9',
+		'qin pivots' => '2, 3, 9',
+		'trio pivots' => '2, 3, 9',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
 		'favorites' => '1, 4, 5, 6',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
 		'official win' => '5, 7, 1, 4',
 		'win amount' => 53.5,
 		'qin amount' => 180.5,
@@ -120,12 +170,19 @@ return [
 			7 => 23.5,
 			1 => 13,
 		],
+		'win inter' => '4',
+		'qin inter' => '1, 4, 6',
+		'trio inter' => '1, 4, 6',
+		'win pivots' => '1, 4, 5, 6',
+		'qin pivots' => '1, 4, 5, 6',
+		'trio pivots' => '1, 4, 5, 6',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'favorites' => '1, 3, 6',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 9, 10, 11',
 		'official win' => '6, 1, 2, 4',
 		'win amount' => 40.5,
 		'qin amount' => 56.5,
@@ -135,12 +192,19 @@ return [
 			1 => 12.5,
 			2 => 24,
 		],
+		'win inter' => '1',
+		'qin inter' => '1, 3',
+		'trio inter' => '1, 3',
+		'win pivots' => '1, 3, 6',
+		'qin pivots' => '1, 3, 6',
+		'trio pivots' => '1, 3, 6',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
 		'favorites' => '1, 2, 7, 8',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
 		'official win' => '7, 8, 9, 1',
 		'win amount' => 30,
 		'qin amount' => 55,
@@ -150,12 +214,19 @@ return [
 			8 => 14,
 			9 => 43.5,
 		],
+		'win inter' => '7',
+		'qin inter' => '2, 7',
+		'trio inter' => '2, 7',
+		'win pivots' => '2, 7',
+		'qin pivots' => '2, 7',
+		'trio pivots' => '1, 2, 7, 8',
 	],
 	'11' => [
 		/**
 		Race 11
 		*/
 		'favorites' => '1, 6, 11, 13',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13',
 		'official win' => '4, 8, 6, 9',
 		'win amount' => 54.5,
 		'qin amount' => 1206.5,
@@ -165,5 +236,16 @@ return [
 			8 => 73.5,
 			6 => 25.5,
 		],
+		'win inter' => '',
+		'qin inter' => '',
+		'trio inter' => '',
+		'win pivots' => '',
+		'qin pivots' => '11',
+		'trio pivots' => '1, 6, 11',
 	],
 ];
+//total win: 0
+//total place: 0
+//total qin: 0
+//total trio: 0
+//total: 0
