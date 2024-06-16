6f82b22ebf4756e593e1aa6dc2cbf05f13143184
artistic software <aadel.iimam@gmail.com>
2024-06-16 08:31:14 +0000

    ...

--- check.php
+++ check.php
@@ -2,6 +2,12 @@
 
 $favoritesWin = [];
 $favoritesPlace = [];
+$winInterWin = [];
+$qinInterWin = [];
+$trioInterWin = [];
+$winInterPlace = [];
+$qinInterPlace = [];
+$trioInterPlace = [];
 $favoritesQin = [];
 $favoritesTrio = [];
 $smallestFavoriteWin = [];
@@ -26,6 +32,7 @@ foreach ($dir as $fileinfo) {
             $runners = array_filter(explode(", ", $data['runners']));
             $winInter = array_filter(explode(", ", $data['win inter']));
             $qinInter = array_filter(explode(", ", $data['qin inter']));
+            $trioInter = array_filter(explode(", ", $data['trio inter']));
             $winPivots = array_filter(explode(", ", $data['win pivots']));
             $qinPivots = array_filter(explode(", ", $data['qin pivots']));
             $trioPivots = array_filter(explode(", ", $data['trio pivots']));
@@ -35,10 +42,34 @@ foreach ($dir as $fileinfo) {
                 if(!isset($favoritesWin[$expr])) $favoritesWin[$expr] = true;
             }
             else $favoritesWin[$expr] = false;
+            if(in_array($winners[0], $winInter)){
+                if(!isset($winInterWin[$expr])) $winInterWin[$expr] = true;
+            }
+            else $winInterWin[$expr] = false;
+            if(in_array($winners[0], $qinInter)){
+                if(!isset($qinInterWin[$expr])) $qinInterWin[$expr] = true;
+            }
+            else $qinInterWin[$expr] = false;
+            if(in_array($winners[0], $trioInter)){
+                if(!isset($trioInterWin[$expr])) $trioInterWin[$expr] = true;
+            }
+            else $trioInterWin[$expr] = false;
             if(!empty(array_intersect($favorites, array_slice($winners, 0, 3)))){
                 if(!isset($favoritesPlace[$expr])) $favoritesPlace[$expr] = true;
             }
             else $favoritesPlace[$expr] = false;
+            if(!empty(array_intersect($favorites, array_slice($winInter, 0, 3)))){
+                if(!isset($winInterPlace[$expr])) $winInterPlace[$expr] = true;
+            }
+            else $winInterPlace[$expr] = false;
+            if(!empty(array_intersect($favorites, array_slice($qinInter, 0, 3)))){
+                if(!isset($qinInterPlace[$expr])) $qinInterPlace[$expr] = true;
+            }
+            else $qinInterPlace[$expr] = false;
+            if(!empty(array_intersect($favorites, array_slice($trioInter, 0, 3)))){
+                if(!isset($trioInterPlace[$expr])) $trioInterPlace[$expr] = true;
+            }
+            else $trioInterPlace[$expr] = false;
             if($winners[0] == $favorites[0]){
                 if(!isset($smallestFavoriteWin[$expr])) $smallestFavoriteWin[$expr] = true;
             }
@@ -84,6 +115,24 @@ foreach($favoritesWin as $key => $value){
 foreach($favoritesPlace as $key => $value){
     if($value === false || empty($key)) unset($favoritesPlace[$key]);
 }
+foreach($winInterWin as $key => $value){
+    if($value === false || empty($key)) unset($winInterWin[$key]);
+}
+foreach($winInterPlace as $key => $value){
+    if($value === false || empty($key)) unset($winInterPlace[$key]);
+}
+foreach($qinInterWin as $key => $value){
+    if($value === false || empty($key)) unset($qinInterWin[$key]);
+}
+foreach($qinInterPlace as $key => $value){
+    if($value === false || empty($key)) unset($qinInterPlace[$key]);
+}
+foreach($trioInterWin as $key => $value){
+    if($value === false || empty($key)) unset($trioInterWin[$key]);
+}
+foreach($trioInterPlace as $key => $value){
+    if($value === false || empty($key)) unset($trioInterPlace[$key]);
+}
 foreach($favoritesQin as $key => $value){
     if($value === false || empty($key)) unset($favoritesQin[$key]);
 }
@@ -103,7 +152,13 @@ foreach($biggestFavoritePlace as $key => $value){
     if($value === false || empty($key)) unset($biggestFavoritePlace[$key]);
 }
 $favoritesWinKeys = array_filter(array_keys($favoritesWin));
+$winInterWinKeys = array_filter(array_keys($winInterWin));
+$qinInterWinKeys = array_filter(array_keys($qinInterWin));
+$trioInterWinKeys = array_filter(array_keys($trioInterWin));
 $favoritesPlaceKeys = array_filter(array_keys($favoritesPlace));
+$winInterPlaceKeys = array_filter(array_keys($winInterPlace));
+$qinInterPlaceKeys = array_filter(array_keys($qinInterPlace));
+$trioInterPlaceKeys = array_filter(array_keys($trioInterPlace));
 $favoritesQinKeys = array_filter(array_keys($favoritesQin));
 $favoritesTrioKeys = array_filter(array_keys($favoritesTrio));
 $biggestFavoriteWinKeys = array_filter(array_keys($biggestFavoriteWin));
@@ -115,7 +170,13 @@ for($k = 1; $k <= 14; $k++){
     sort($placeKeys[$k]);
 }
 sort($favoritesWinKeys);
+sort($winInterWinKeys);
+sort($qinInterWinKeys);
+sort($trioInterWinKeys);
 sort($favoritesPlaceKeys);
+sort($winInterPlaceKeys);
+sort($qinInterPlaceKeys);
+sort($trioInterPlaceKeys);
 sort($favoritesQinKeys);
 sort($favoritesTrioKeys);
 sort($smallestFavoriteWinKeys);
@@ -123,7 +184,13 @@ sort($biggestFavoriteWinKeys);
 sort($smallestFavoritePlaceKeys);
 sort($biggestFavoritePlaceKeys);
 $outtext .= '$favoriteWin = ' . "[" . implode(", ", array_filter($favoritesWinKeys)) . "];\n";
+$outtext .= '$winInterWin = ' . "[" . implode(", ", array_filter($winInterWinKeys)) . "];\n";
+$outtext .= '$qinInterWin = ' . "[" . implode(", ", array_filter($qinInterWinKeys)) . "];\n";
+$outtext .= '$trioInterWin = ' . "[" . implode(", ", array_filter($trioInterWinKeys)) . "];\n";
 $outtext .= '$favoritePlace = ' . "[" . implode(", ", array_filter($favoritesPlaceKeys)) . "];\n";
+$outtext .= '$winInterPlace = ' . "[" . implode(", ", array_filter($winInterPlaceKeys)) . "];\n";
+$outtext .= '$qinInterPlace = ' . "[" . implode(", ", array_filter($qinInterPlaceKeys)) . "];\n";
+$outtext .= '$trioInterPlace = ' . "[" . implode(", ", array_filter($trioInterPlaceKeys)) . "];\n";
 $outtext .= '$favoriteQin =' . " [" . implode(", ", array_filter($favoritesQinKeys)) . "];\n";
 $outtext .= '$favoriteTrio =' . " [" . implode(", ", array_filter($favoritesTrioKeys)) . "];\n";
 $outtext .= '$smallestFavoriteWin =' . " [" . implode(", ", array_filter($smallestFavoriteWinKeys)) . "];\n";
--- condition.php
+++ condition.php
@@ -2,7 +2,13 @@
 
 
 $favoriteWin = [5, 10, 24];
+$winInterWin = [];
+$qinInterWin = [24];
+$trioInterWin = [24];
 $favoritePlace = [5, 10, 14, 16, 18, 20, 24];
+$winInterPlace = [12, 16, 18, 20];
+$qinInterPlace = [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 24];
+$trioInterPlace = [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 24];
 $favoriteQin = [];
 $favoriteTrio = [];
 $smallestFavoriteWin = [];
