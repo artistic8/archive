3329bca31c87a15dbea9f2ce317663326dda1151
adilelbourichi <adilelbourichi@gmail.com>
2024-06-25 02:22:50 +0000

    ...

--- condition.php
+++ condition.php
@@ -1,16 +1,15 @@
 <?php
 
 
-$favoriteWin = [19, 20];
-$nonFavoriteWin = [];
-$favoritePlace = [1, 8, 13, 14, 19, 20];
-$allValuesWin = [3, 5, 9, 13, 14, 19, 20];
-$allValuesPlace = [1, 2, 3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 19, 20, 21];
-$allValuesQin = [3, 13, 14, 19, 20];
-$allValuesTrio = [13, 20];
-$smallestFavoriteWin = [];
+$favoriteWin = [5, 18, 20];
+$favoritePlace = [1, 5, 8, 13, 14, 18, 19, 20];
+$allValuesWin = [3, 5, 11, 13, 14, 18, 19, 20];
+$allValuesPlace = [1, 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21];
+$allValuesQin = [3, 13, 18, 20];
+$allValuesTrio = [18, 20];
+$smallestFavoriteWin = [18];
 $biggestFavoriteWin = [];
-$smallestFavoritePlace = [];
-$biggestFavoritePlace = [13, 14];
+$smallestFavoritePlace = [18];
+$biggestFavoritePlace = [];
 
 ?>
