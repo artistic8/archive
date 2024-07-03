15664d138739142f5520e9407a830d565dcf9940
artistic software <aadel.iimam@gmail.com>
2024-07-03 05:27:42 +0000

    ...

--- sets.php
+++ sets.php
@@ -134,7 +134,10 @@ for ($raceNumber = 1; $raceNumber <= $numberOfRaces; $raceNumber++) {
     }
     
     sort($values);
-    if(!empty($values)) $racetext .= "\t\t'values' => '" . implode(", ", $values) . "',\n";
+    if(!empty($values)) {
+        $racetext .= "\t\t'values' => '" . implode(", ", $values) . "',\n";
+        $racetext .= "\t\t'official win' => '" . implode(", ", $officialWin) . "',\n";
+    }
     $racetext .= "\t],\n";
     unset($oldFavorites);
     unset($favorites);
