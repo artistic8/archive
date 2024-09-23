88171f7ec584b2e3937f122f2ae04982a6eb0ca3
artistic software <aadel.iimam@gmail.com>
2024-09-23 05:26:17 +0000

    ...

---
+++ 20240922/agregate.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 2, 7, 9',
+		'place(end-favorites , $100)' => '9',
+		'places' => '9',
+		'sures' => '9',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 1, 4, 6, 8',
+		'places' => '6, 8',
+		'place(end-wp 1026, $100)' => '8',
+		'place(end-wp 1031, $100)' => '6',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 4, 5',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 1, 5',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 2',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 1, 3, 4, 8',
+		'place(end-favorites , $100)' => '8',
+		'places' => '8',
+		'sures' => '8',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 1, 6, 7',
+		'place(end-favorites , $100)' => '7',
+		'places' => '7',
+		'sures' => '7',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 3, 7',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 1, 4, 5, 9',
+		'place(end-favorites , $100)' => '9',
+		'places' => '5, 9',
+		'sures' => '9',
+		'place(end-wp 1099, $100)' => '5',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '(F) 2, 5, 6, 10, 11',
+	],
+];
