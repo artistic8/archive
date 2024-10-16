d797f93e0b4fea1a5b74c220f09f3a7cfd242cc5
artistic software <aadel.iimam@gmail.com>
2024-10-16 06:06:49 +0000

    ...

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
-index=52;
-for f in *; do
-    if [ -d "$f" ] && [ $f != "archive" ]; then
+index=1;
+for f in 2024*; do
+    if [ -d "$f" ]; then
         rm -Rf $f;
-        for g in *; do
-            if [ -d "$g" ] && [ $g != "archive" ]; then
-                rm -Rf $g;
+        # for g in *; do
+            # if [ -d "$g" ] && [ $g != "archive" ]; then
+                # rm -Rf $g;
                 php populate.php;
                 cp history.php history$index.php;
-                git checkout $g;
+                # git checkout $g;
                 git checkout history.php;
                 index=$((index+1));
-            fi
-        done
+            # fi
+        # done
         git checkout $f/;
     fi
 done
