d254e5db5716b7828b5c84ebbbd5d4ec6e7a7b19
artistic software <aadel.iimam@gmail.com>
2024-10-31 02:40:51 +0000

    ...

---
+++ 20241030/agregate.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 1, 6, 7',
+		'place(end-favorites )' => '7',
+		'super sure bet' => 'super sure place 7',
+		'places($10)' => '7',
+		'sures($10)' => '7',
+		'super sures($10)' => '7',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 1, 2, 9, 10',
+		'place(end-favorites )' => '10',
+		'places($10)' => '9, 10',
+		'sures($10)' => '10',
+		'placesWP($30)' => '10, 1, 9',
+		'unions($10)' => '1, 4, 5, 9, 10',
+		'union + favorites' => '1, 2, 4, 5, 9, 10',
+		'place(end-wp 1006)' => '9',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 3, 5, 6, 7, 8',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 2, 3, 4',
+		'place(end-favorites )' => '4',
+		'super sure bet' => 'super sure place 4',
+		'places($10)' => '4',
+		'sures($10)' => '4',
+		'super sures($10)' => '4',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 1, 3, 5, 8, 11',
+		'placesWP($30)' => '1',
+		'unions($10)' => '1, 4, 5, 8',
+		'union + favorites' => '1, 3, 4, 5, 8, 11',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 2, 5, 6',
+		'place(end-favorites )' => '6',
+		'super sure bet' => 'super sure place 6',
+		'places($10)' => '6',
+		'sures($10)' => '6',
+		'super sures($10)' => '6',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 5, 8, 11',
+		'win(union )' => '5, 6, 8, 11',
+		'unions($10)' => '5, 6, 8, 11',
+		'union + favorites' => '5, 6, 8, 11',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 1, 2, 3, 5, 9, 10',
+		'unions($10)' => '3, 5, 8, 10',
+		'union + favorites' => '1, 2, 3, 5, 8, 9, 10',
+	],
+];
---
+++ 20241030/bets.php
@@ -0,0 +1,124 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '1, 6, 7',
+		'official win' => '9, 7, 11, 1',
+		'win amount' => 136,
+		'qin amount' => 232,
+		'trio amount' => 297,
+		'place amount' => [
+			9 => 32,
+			7 => 15,
+			11 => 17.5,
+		],
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '1, 2, 9, 10',
+		'official win' => '10, 12, 8, 9',
+		'win amount' => 100.5,
+		'qin amount' => 793.5,
+		'trio amount' => 3758,
+		'place amount' => [
+			10 => 29,
+			12 => 42,
+			8 => 38.5,
+		],
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '3, 5, 6, 7, 8',
+		'official win' => '8, 3, 6, 11',
+		'win amount' => 40.5,
+		'qin amount' => 103,
+		'trio amount' => 481,
+		'place amount' => [
+			8 => 15.5,
+			3 => 18.5,
+			6 => 50,
+		],
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '2, 3, 4',
+		'official win' => '2, 4, 6, 5',
+		'win amount' => 44,
+		'qin amount' => 204,
+		'trio amount' => 887,
+		'place amount' => [
+			2 => 18.5,
+			4 => 25,
+			6 => 40.5,
+		],
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '1, 3, 5, 8, 11',
+		'official win' => '2, 3, 10, 11',
+		'win amount' => 131.5,
+		'qin amount' => 2061,
+		'trio amount' => 3578,
+		'place amount' => [
+			2 => 49.5,
+			3 => 87,
+			10 => 21,
+		],
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '2, 5, 6',
+		'official win' => '5, 6, 10, 2',
+		'win amount' => 41,
+		'qin amount' => 100,
+		'trio amount' => 603,
+		'place amount' => [
+			5 => 18.5,
+			6 => 15,
+			10 => 45,
+		],
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '5, 8, 11',
+		'official win' => '9, 10, 11, 3',
+		'win amount' => 82,
+		'qin amount' => 245.5,
+		'trio amount' => 154,
+		'place amount' => [
+			9 => 24.5,
+			10 => 21,
+			11 => 10.5,
+		],
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '1, 2, 3, 5, 9, 10',
+		'official win' => '2, 9, 3, 4',
+		'win amount' => 53,
+		'qin amount' => 88,
+		'trio amount' => 124,
+		'place amount' => [
+			2 => 18.5,
+			9 => 14.5,
+			3 => 15.5,
+		],
+	],
+];
