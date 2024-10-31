d3e3a6214f9526779c0bfb1ca7ce0a4ebf90ed4c
artistic software <aadel.iimam@gmail.com>
2024-10-31 02:53:03 +0000

    ...

--- script.sh
+++ script.sh
@@ -3,9 +3,11 @@ for f in *; do
         rm -Rf $f;
         php populate.php;
         php favpopulate.php;
+        php allfavpopulate.php;
         git checkout $f/;
         php bets.php $f;
         git checkout history.php;
         git checkout favhistory.php;
+        git checkout allfavhistory.php;
     fi
 done
