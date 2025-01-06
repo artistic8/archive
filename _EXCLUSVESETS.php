8732511fec10722d643516eb79300408cc214485
artistic software <aadel.iimam@gmail.com>
2025-01-06 04:50:22 +0000

    ...

---
+++ _EXCLUSVESETS.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * TODO:
+ * Looping through all previous results (2024, 2023, 2022 ....), find exclusive sets A1, A2... s.t. Ai inter Aj = empty and for all results p, q, r we have
+ * p is in Ai
+ * q is in Aj
+ * r is in Ak
+ * 
+ */
+
+
+?>
