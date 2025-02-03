e15f8e3dd6ac5e2ac442ea6bcd80ed60fcd86fa9
artistic3 <artisticsoftware3@gmail.com>
2025-02-03 12:45:35 +0800

    ...

--- script.sh
+++ script.sh
@@ -2,8 +2,6 @@ for f in *; do
     if [ -d "$f" ] && [ $f != "archive" ] && [ $f != "oldFavorites" ]; then
         rm -Rf $f;
         php populate.php;
-        php minpopulate.php;
-        php maxpopulate.php;
         php favpopulate.php;
         php allfavpopulate.php;
         git checkout $f/;
