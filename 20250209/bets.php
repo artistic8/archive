8504eacdd8d929b07748893db4adafdeebb4cc28
artistic8 <aadel.iimam@gmail.com>
2025-02-10 16:47:03 +0800

    ...

---
+++ 20250209/bets.php
@@ -0,0 +1,169 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '1, 13',
+		'official win' => '6, 2, 13, 7',
+		'win amount' => 207,
+		'qin amount' => 671.5,
+		'trio amount' => 546,
+		'place amount' => [
+			6 => 51,
+			2 => 21,
+			13 => 13,
+		],
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2, 9, 14',
+		'official win' => '9, 2, 7, 3',
+		'win amount' => 62,
+		'qin amount' => 130,
+		'trio amount' => 327,
+		'place amount' => [
+			9 => 19.5,
+			2 => 16,
+			7 => 34.5,
+		],
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '1, 2, 3, 10',
+		'official win' => '3, 1, 2, 14',
+		'win amount' => 73.5,
+		'qin amount' => 114.5,
+		'trio amount' => 274,
+		'place amount' => [
+			3 => 25.5,
+			1 => 12.5,
+			2 => 42,
+		],
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '1, 2, 4, 11',
+		'official win' => '11, 7, 12, 4',
+		'win amount' => 102.5,
+		'qin amount' => 821,
+		'trio amount' => 1573,
+		'place amount' => [
+			11 => 36,
+			7 => 38,
+			12 => 23,
+		],
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '1, 3, 11',
+		'official win' => '7, 4, 3, 1',
+		'win amount' => 174.5,
+		'qin amount' => 927,
+		'trio amount' => 690,
+		'place amount' => [
+			7 => 35.5,
+			4 => 39.5,
+			3 => 12,
+		],
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '2, 4, 6, 8, 11',
+		'official win' => '4, 11, 6, 7',
+		'win amount' => 168,
+		'qin amount' => 1222,
+		'trio amount' => 1124,
+		'place amount' => [
+			4 => 36.5,
+			11 => 44,
+			6 => 13,
+		],
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '3, 7, 12',
+		'official win' => '7, 12, 9, 2',
+		'win amount' => 20,
+		'qin amount' => 43.5,
+		'trio amount' => 188,
+		'place amount' => [
+			7 => 11,
+			12 => 16,
+			9 => 38,
+		],
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '2, 3, 7, 8',
+		'official win' => '4, 6, 5, 3',
+		'win amount' => 108.5,
+		'qin amount' => 277,
+		'trio amount' => 455,
+		'place amount' => [
+			4 => 26,
+			6 => 17.5,
+			5 => 19.5,
+		],
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '3, 4, 8',
+		'official win' => '3, 7, 11, 12',
+		'win amount' => 69,
+		'qin amount' => 390.5,
+		'trio amount' => 1745,
+		'place amount' => [
+			3 => 20,
+			7 => 47,
+			11 => 37,
+		],
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '2, 4',
+		'official win' => '12, 4, 11, 3',
+		'win amount' => 128.5,
+		'qin amount' => 148.5,
+		'trio amount' => 368,
+		'place amount' => [
+			12 => 27.5,
+			4 => 13,
+			11 => 23,
+		],
+	],
+	'11' => [
+		/**
+		Race 11
+		*/
+		'favorites' => '2, 3',
+		'official win' => '5, 2, 14, 7',
+		'win amount' => 108.5,
+		'qin amount' => 113.5,
+		'trio amount' => 2836,
+		'place amount' => [
+			5 => 29.5,
+			2 => 11,
+			14 => 195,
+		],
+	],
+];
