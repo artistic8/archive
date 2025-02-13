4262fca0163d340db8b79b7d3e3a279475adddf4
artistic8 <aadel.iimam@gmail.com>
2025-02-13 14:10:24 +0800

    ...

--- 20250131/agregate.php
+++ 20250131/agregate.php
@@ -18,33 +18,23 @@ return [
 		Race 3
 		*/
 		'favorites' => '(F) 3, 9, 13',
-		'place(end-favorites )' => '13',
-		'places($10)' => '13',
-		'sures($10)' => '13',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'favorites' => '(F) 2, 4, 6, 10',
-		'places($10)' => '10',
-		'sures($10)' => '10',
 		'unions($10)' => '1, 2, 4, 5, 10',
 		'union + favorites' => '1, 2, 4, 5, 6, 10',
-		'early 4' => '',
-		'early 5' => '1, 2, 4, 5, 10',
-		'super sures($10)' => '10',
+		'official win' => '4, 11, 6, 9',
+		'win amount' => 70.5,
+		'qin amount' => 982.5,
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
 		'favorites' => '(F) 4, 6, 7',
-		'place(end-favorites )' => '7',
-		'super sure bet' => 'super sure place 7',
-		'places($10)' => '7',
-		'sures($10)' => '7',
-		'super sures($10)' => '7',
 	],
 	'6' => [
 		/**
@@ -57,16 +47,11 @@ return [
 		Race 7
 		*/
 		'favorites' => '(F) 4, 7, 8',
-		'all fav history' => '(A) 3, 5, 8',
-		'place(end-favorites )' => '8',
-		'super sure bet' => 'super sure place 8',
-		'places($10)' => '8',
-		'sures($10)' => '8',
 		'unions($10)' => '2, 4, 7, 8',
 		'union + favorites' => '2, 4, 7, 8',
-		'early 4' => '2, 4, 7, 8',
-		'early 5' => '2, 4, 7, 8',
-		'super sures($10)' => '8',
+		'official win' => '7, 3, 1, 14',
+		'win amount' => 24.5,
+		'qin amount' => 136,
 	],
 	'8' => [
 		/**
@@ -75,8 +60,9 @@ return [
 		'favorites' => '(F) 1, 3, 6, 7',
 		'unions($10)' => '1, 4, 6, 7',
 		'union + favorites' => '1, 3, 4, 6, 7',
-		'early 4' => '1, 4, 6, 7',
-		'early 5' => '1, 3, 4, 6, 7',
+		'official win' => '11, 3, 1, 5',
+		'win amount' => 70.5,
+		'qin amount' => 1416,
 	],
 	'9' => [
 		/**
@@ -91,8 +77,9 @@ return [
 		'favorites' => '(F) 3, 4, 8, 11',
 		'unions($10)' => '1, 3, 8, 11, 12, 14',
 		'union + favorites' => '1, 3, 4, 8, 11, 12, 14',
-		'early 4' => '',
-		'early 5' => '1, 3, 11, 12, 14',
+		'official win' => '2, 11, 8, 5',
+		'win amount' => 422.5,
+		'qin amount' => 875,
 	],
 	'11' => [
 		/**
--- 20250205/agregate.php
+++ 20250205/agregate.php
@@ -8,8 +8,9 @@ return [
 		'favorites' => '(F) 4, 8',
 		'unions($10)' => '1, 3, 4, 8',
 		'union + favorites' => '1, 3, 4, 8',
-		'early 4' => '1, 3, 4, 8',
-		'early 5' => '1, 3, 4, 8',
+		'official win' => '4, 8, 5, 11',
+		'win amount' => 90.5,
+		'qin amount' => 238,
 	],
 	'2' => [
 		/**
@@ -28,41 +29,33 @@ return [
 		Race 4
 		*/
 		'favorites' => '(F) 1, 2, 8',
-		'place(end-favorites )' => '8',
-		'super sure bet' => 'super sure place 8',
-		'places($10)' => '8',
-		'sures($10)' => '8',
 		'unions($10)' => '1, 2, 3, 5, 8',
 		'union + favorites' => '1, 2, 3, 5, 8',
-		'early 4' => '',
-		'early 5' => '1, 2, 3, 5, 8',
-		'super sures($10)' => '8',
+		'official win' => '5, 7, 2, 3',
+		'win amount' => 122.5,
+		'qin amount' => 875.5,
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
 		'favorites' => '(F) 1, 2, 3, 5',
-		'place(end-favorites )' => '5',
-		'super sure bet' => 'super sure place 5',
-		'places($10)' => '5',
-		'sures($10)' => '5',
 		'unions($10)' => '1, 2, 3, 4, 5, 9',
 		'union + favorites' => '1, 2, 3, 4, 5, 9',
-		'early 4' => '',
-		'early 5' => '1, 2, 4, 5, 9',
-		'super sures($10)' => '5',
+		'official win' => '5, 2, 4, 10',
+		'win amount' => 40,
+		'qin amount' => 55,
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'favorites' => '(F) 2, 7',
-		'all fav history' => '(A) 1, 5, 7',
 		'unions($10)' => '1, 2, 3, 7, 8, 9',
 		'union + favorites' => '1, 2, 3, 7, 8, 9',
-		'early 4' => '',
-		'early 5' => '',
+		'official win' => '2, 9, 4, 7',
+		'win amount' => 39.5,
+		'qin amount' => 321.5,
 		'win(union 1351)' => '2, 3, 7, 8',
 		'win(union 1378)' => '2, 3, 7, 9',
 		'win(union 1547)' => '1, 2, 3, 7',
@@ -72,11 +65,6 @@ return [
 		Race 7
 		*/
 		'favorites' => '(F) 1, 2, 4',
-		'place(end-favorites )' => '4',
-		'super sure bet' => 'super sure place 4',
-		'places($10)' => '4',
-		'sures($10)' => '4',
-		'super sures($10)' => '4',
 	],
 	'8' => [
 		/**
