d1ddd389f27c2f4361a39ea7d3c006df792edd8d
artistic8 <aadel.iimam@gmail.com>
2025-02-13 08:51:20 +0800

    ...

--- 20250126/agregate.php
+++ 20250126/agregate.php
@@ -8,33 +8,31 @@ return [
 		'favorites' => '(F) 1, 2, 11, 13',
 		'unions($10)' => '1, 2, 5, 11, 13',
 		'union + favorites' => '1, 2, 5, 11, 13',
-		'early 4' => '2, 5, 11, 13',
-		'early 5' => '1, 2, 5, 11, 13',
+		'official win' => '3, 2, 11, 10',
+		'win amount' => 116.5,
+		'qin amount' => 178,
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'favorites' => '(F) 1, 4, 8, 13',
-		'places($10)' => '8',
-		'sures($10)' => '8',
 		'unions($10)' => '1, 2, 4, 8, 9',
 		'union + favorites' => '1, 2, 4, 8, 9, 13',
-		'early 4' => '',
-		'early 5' => '1, 2, 4, 8, 9',
-		'super sures($10)' => '8',
+		'official win' => '8, 13, 11, 6',
+		'win amount' => 93,
+		'qin amount' => 146.5,
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'favorites' => '(F) 2, 3, 4, 5, 6, 8',
-		'places($10)' => '5, 6',
 		'unions($10)' => '1, 2, 4, 5, 6, 7',
 		'union + favorites' => '1, 2, 3, 4, 5, 6, 7, 8',
-		'early 4' => '',
-		'early 5' => '',
-		'super sures($10)' => '6',
+		'official win' => '4, 8, 3, 7',
+		'win amount' => 14.5,
+		'qin amount' => 132,
 	],
 	'4' => [
 		/**
@@ -43,8 +41,9 @@ return [
 		'favorites' => '(F) 1, 2, 6, 13',
 		'unions($10)' => '2, 6, 12, 13',
 		'union + favorites' => '1, 2, 6, 12, 13',
-		'early 4' => '2, 6, 12, 13',
-		'early 5' => '1, 2, 6, 12, 13',
+		'official win' => '6, 2, 13, 8',
+		'win amount' => 20,
+		'qin amount' => 50.5,
 	],
 	'5' => [
 		/**
@@ -54,8 +53,9 @@ return [
 		'places($10)' => '5, 13',
 		'unions($10)' => '1, 2, 4, 5, 9, 13',
 		'union + favorites' => '1, 2, 4, 5, 8, 9, 13',
-		'early 4' => '',
-		'early 5' => '1, 2, 4, 5, 9',
+		'official win' => '11, 10, 12, 13',
+		'win amount' => 67.5,
+		'qin amount' => 615,
 	],
 	'6' => [
 		/**
@@ -88,8 +88,9 @@ return [
 		'favorites' => '(F) 6, 9',
 		'unions($10)' => '3, 6, 9, 10',
 		'union + favorites' => '3, 6, 9, 10',
-		'early 4' => '3, 6, 9, 10',
-		'early 5' => '3, 6, 9, 10',
+		'official win' => '6, 5, 3, 8',
+		'win amount' => 22,
+		'qin amount' => 829.5,
 	],
 	'11' => [
 		/**
