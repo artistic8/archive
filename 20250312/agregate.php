7023707b9cac1fdd071a2fd48bac202ab33aae76
artistic8 <aadel.iimam@gmail.com>
2025-03-13 09:40:57 +0800

    ...

---
+++ 20250312/agregate.php
@@ -0,0 +1,94 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 1, 2, 7, 9, 11, 12',
+		'unions($10)' => '2, 8, 9, 10, 12',
+		'union + favorites' => '1, 2, 7, 8, 9, 10, 11, 12',
+		'count union + favorites' => '8',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 1, 5, 6, 11',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 2, 4, 7, 11',
+		'unions($10)' => '2, 5, 7, 11',
+		'union + favorites' => '2, 4, 5, 7, 11',
+		'count union + favorites' => '5',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 1, 2, 4, 5, 8, 9, 10',
+		'qin(seven )' => '1, 2, 4, 5, 8, 9, 10',
+		'unions($10)' => '2, 4, 5, 9',
+		'union + favorites' => '1, 2, 4, 5, 8, 9, 10',
+		'count union + favorites' => '7',
+		'sevens($10)' => '1, 2, 4, 5, 8, 9, 10',
+		'count sevens' => '7',
+		'inter sevens unions' => '2, 4, 5, 9',
+		'count inter sevens unions' => '4',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 1, 4, 8',
+		'qin(union )' => '1, 4, 5, 8',
+		'unions($10)' => '1, 3, 4, 5, 8',
+		'union + favorites' => '1, 3, 4, 5, 8',
+		'count union + favorites' => '5',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 2, 3, 7, 8, 10, 11',
+		'qin(seven )' => '1, 2, 3, 7, 8, 10, 11',
+		'unions($10)' => '1, 2, 3, 7, 8, 10',
+		'union + favorites' => '1, 2, 3, 7, 8, 10, 11',
+		'count union + favorites' => '7',
+		'sevens($10)' => '1, 2, 3, 4, 7, 8, 10, 11',
+		'count sevens' => '8',
+		'inter sevens unions' => '1, 2, 3, 7, 8, 10',
+		'count inter sevens unions' => '6',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 2, 6, 7',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 1, 3, 5, 6, 10',
+		'unions($10)' => '1, 3, 4, 5, 6, 10',
+		'union + favorites' => '1, 3, 4, 5, 6, 10',
+		'count union + favorites' => '6',
+		'sevens($10)' => '1, 2, 3, 4, 5, 7, 10',
+		'count sevens' => '7',
+		'inter sevens unions' => '1, 3, 4, 5, 10',
+		'count inter sevens unions' => '5',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 3, 4, 8, 10, 11',
+		'unions($10)' => '4, 5, 8, 10',
+		'union + favorites' => '3, 4, 5, 8, 10, 11',
+		'count union + favorites' => '6',
+	],
+];
---
+++ 20250312/bets.php
@@ -0,0 +1,161 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '1, 2, 7, 9, 11, 12',
+		'official win' => '1, 11, 7, 12',
+		'union + favorites' => '1, 2, 7, 8, 9, 10, 11, 12',
+		'count union + favorites' => '8',
+		'win amount' => 90.5,
+		'qin amount' => 569,
+		'trio amount' => 975,
+		'place amount' => [
+			1 => 28,
+			11 => 40,
+			7 => 18,
+		],
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '1, 5, 6, 11',
+		'official win' => '6, 11, 5, 3',
+
+		'win amount' => 56,
+		'qin amount' => 120,
+		'trio amount' => 327,
+		'place amount' => [
+			6 => 19.5,
+			11 => 16.5,
+			5 => 26,
+		],
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '2, 4, 7, 11',
+		'official win' => '7, 3, 2, 10',
+		'union + favorites' => '2, 4, 5, 7, 11',
+		'count union + favorites' => '5',
+		'win amount' => 50,
+		'qin amount' => 485,
+		'trio amount' => 1369,
+		'place amount' => [
+			7 => 18,
+			3 => 54,
+			2 => 25.5,
+		],
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '1, 2, 4, 5, 8, 9, 10',
+		'official win' => '1, 7, 12, 2',
+		'union + favorites' => '1, 2, 4, 5, 8, 9, 10',
+		'count union + favorites' => '7',
+		'sevens($10)' => '1, 2, 4, 5, 8, 9, 10',
+		'count sevens' => '7',
+		'win amount' => 46.5,
+		'qin amount' => 467.5,
+		'trio amount' => 1201,
+		'place amount' => [
+			1 => 20.5,
+			7 => 49.5,
+			12 => 24.5,
+		],
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '1, 4, 8',
+		'official win' => '6, 1, 2, 7',
+		'union + favorites' => '1, 3, 4, 5, 8',
+		'count union + favorites' => '5',
+		'win amount' => 61,
+		'qin amount' => 175,
+		'trio amount' => 318,
+		'place amount' => [
+			6 => 28,
+			1 => 18,
+			2 => 20,
+		],
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '2, 3, 7, 8, 10, 11',
+		'official win' => '10, 9, 7, 8',
+		'union + favorites' => '1, 2, 3, 7, 8, 10, 11',
+		'count union + favorites' => '7',
+		'sevens($10)' => '1, 2, 3, 4, 7, 8, 10, 11',
+		'count sevens' => '8',
+		'win amount' => 125.5,
+		'qin amount' => 324,
+		'trio amount' => 279,
+		'place amount' => [
+			10 => 25.5,
+			9 => 29.5,
+			7 => 12,
+		],
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '2, 6, 7',
+		'official win' => '7, 6, 11, 2',
+
+		'win amount' => 116,
+		'qin amount' => 100.5,
+		'trio amount' => 139,
+		'place amount' => [
+			7 => 25,
+			6 => 11,
+			11 => 21.5,
+		],
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '1, 3, 5, 6, 10',
+		'official win' => '5, 4, 1, 10',
+		'union + favorites' => '1, 3, 4, 5, 6, 10',
+		'count union + favorites' => '6',
+		'sevens($10)' => '1, 2, 3, 4, 5, 7, 10',
+		'count sevens' => '7',
+		'win amount' => 32,
+		'qin amount' => 166,
+		'trio amount' => 366,
+		'place amount' => [
+			5 => 12.5,
+			4 => 29,
+			1 => 27.5,
+		],
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '3, 4, 8, 10, 11',
+		'official win' => '10, 6, 11, 9',
+		'union + favorites' => '3, 4, 5, 8, 10, 11',
+		'count union + favorites' => '6',
+		'win amount' => 21,
+		'qin amount' => 94,
+		'trio amount' => 622,
+		'place amount' => [
+			10 => 11.5,
+			6 => 21.5,
+			11 => 53,
+		],
+	],
+];
