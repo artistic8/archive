2397a3513c33f7424e1eb76d3d2999dcd5677d43
artistic8 <aadel.iimam@gmail.com>
2025-03-20 09:20:18 +0800

    ...

---
+++ 20250319/agregate.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 3, 4, 10',
+		'qin(union )' => '3, 4, 8, 10',
+		'unions($10)' => '3, 4, 8, 10',
+		'union + favorites' => '3, 4, 8, 10',
+		'count union + favorites' => '4',
+	],
+];
---
+++ 20250319/bets.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '3, 4, 10',
+		'official win' => '8, 1, 11, 3',
+		'win amount' => 113.5,
+		'qin amount' => 828,
+		'trio amount' => 1551,
+		'place amount' => [
+			8 => 26.5,
+			1 => 40,
+			11 => 18.5,
+		],
+	],
+];
